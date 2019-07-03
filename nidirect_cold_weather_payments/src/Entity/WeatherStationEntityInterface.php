5f38e0db0283925e7018cb488137663eab865eb7
Steve Cousins <me@stevecousins.co.uk>
2019-07-03 14:18:49 +0100

    D8NID-104 ~ CWP module and Weather Station entity

---
+++ nidirect_cold_weather_payments/src/Entity/WeatherStationEntityInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\nidirect_cold_weather_payments\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining Weather station entities.
+ */
+interface WeatherStationEntityInterface extends ConfigEntityInterface {
+
+  // Add get/set methods for your configuration properties here.
+}

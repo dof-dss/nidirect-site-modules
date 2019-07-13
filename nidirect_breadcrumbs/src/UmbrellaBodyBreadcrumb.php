73eb0b7a8d22c3ceb883049a077c05a21b8a1f84
Johan Gant <johan.gant@gmail.com>
2019-07-13 10:36:42 +0100

    Add breadcrumb handler for umbrella body nodes

---
+++ nidirect_breadcrumbs/src/UmbrellaBodyBreadcrumb.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\nidirect_breadcrumbs;
+
+/**
+ * @file
+ * Generates the breadcrumb trail for content including:
+ * - Umbrella body
+ *
+ * In the format:
+ * > Home
+ * > Crime, justice and the law
+ * > AccessNI criminal record checks
+ * > Find an AccessNI umbrella body
+ *
+ * > <front>
+ * > information-and-services/crime-justice-and-law
+ * > information-and-services/crime-justice-and-law/accessni-criminal-record-checks
+ * > accessni/find-an-umbrella-body
+ */
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+class UmbrellaBodyBreadcrumb implements BreadcrumbBuilderInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $match = FALSE;
+
+    if ($node = $route_match->getParameter('node')) {
+      $match = $node->bundle() == 'umbrella_body';
+    }
+
+    return $match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+
+    $breadcrumb = new Breadcrumb();
+    $links[] = Link::createFromRoute(t('Home'), '<front>');
+    $links[] = Link::fromTextandUrl(t('Crime, justice and the law'), Url::fromUri('entity:taxonomy_term/24'));
+    $links[] = Link::fromTextandUrl(t('AccessNI criminal record checks'), Url::fromUri('entity:node/4007'));
+    $links[] = Link::fromTextandUrl(t('Find an AccessNI umbrella body'), Url::fromUri('entity:node/7712'));
+
+    $breadcrumb->setLinks($links);
+    $breadcrumb->addCacheContexts(['url.path']);
+
+    return $breadcrumb;
+  }
+
+}

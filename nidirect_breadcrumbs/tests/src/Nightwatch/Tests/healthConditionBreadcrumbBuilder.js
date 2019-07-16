12841cd28f6cdfeeb7af95351bcd60f2b254dd8e
Johan Gant <johan.gant@gmail.com>
2019-07-16 15:29:10 +0100

    Breadcrumb builder services for content types with nightwatch tests to validate rendering based on source data

---
+++ nidirect_breadcrumbs/tests/src/Nightwatch/Tests/healthConditionBreadcrumbBuilder.js
@@ -0,0 +1,43 @@
+var parser = require('xml2json');
+var http = require('http');
+var nid, node;
+const breadcrumbSegments = [
+  'Home',
+  'Health and wellbeing',
+  'Illnesses and conditions',
+  'A to Z',
+];
+
+module.exports = {
+  '@tags': ['nidirect-breadcrumbs'],
+
+  before: function (browser) {
+    // GP Practice nodes.
+    http.get(process.env.TEST_D7_URL + '/migrate/healthcond', (response) => {
+      let data = '';
+      response.on('data', (chunk) => { data += chunk });
+
+      response.on('end', () => {
+        data = JSON.parse(parser.toJson(data));
+        node = data.nodes.node;
+        nid = node.nid;
+      })
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+  },
+
+  'Test that Health condition node shows correct breadcrumb pattern': browser => {
+    browser
+      .pause(2000, function () {
+        browser
+          .drupalRelativeURL('/node/' + nid)
+          .waitForElementVisible('body', 1000);
+
+        breadcrumbSegments.forEach(function (item) {
+          browser.expect.element('nav.breadcrumb').text.to.contain(item);
+        });
+      })
+  }
+
+};

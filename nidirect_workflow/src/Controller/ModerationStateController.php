764c81b7eb30c78e1ec8cf818238f556c45b00b5
duttonm <martin@theduttons.org>
2019-07-18 16:36:40 +0100

    add parameters and make state change

--- nidirect_workflow/src/Controller/ModerationStateController.php
+++ nidirect_workflow/src/Controller/ModerationStateController.php
@@ -11,15 +11,30 @@ use Drupal\Core\Url;
 class ModerationStateController extends ControllerBase {
 
   /**
-   * Change_state.
+   * Change_state of specified entity.
    *
    */
-  public function change_state() {
+  public function change_state($nid, $new_state) {
+    // Load the entity.
+    $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+    if ($entity) {
+      // Request the state change.
+      $entity->set('moderation_state', $new_state);
+      $entity->save();
+      // Log it.
+      $message = t('State of') . ' (' . $nid . ') ' . t('changed to' );
+      $message .= ' ' . $new_state . ' ' . t('by') . ' ' . $this->currentUser()->getAccountName();
+      \Drupal::logger('nidirect_workflow')->notice($message);
+    }
     // Redirect user to page given in the 'destination' URL argument.
     $destination = Url::fromUserInput(\Drupal::destination()->get());
     if ($destination->isRouted()) {
       // Valid internal path.
       return $this->redirect($destination->getRouteName());
+    } else {
+      // Route not found, error.
+      $message = "Unable to retrieve route for destination param - " . \Drupal::destination()->get();
+      \Drupal::logger('nidirect_workflow')->error(t($message));
     }
   }
 

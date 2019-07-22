e756d0a087934848485a11b4b690f8e6e91d6427
duttonm <martin@theduttons.org>
2019-07-22 11:36:46 +0100

    Inject EntityTypeManager

--- nidirect_workflow/src/Controller/ModerationStateController.php
+++ nidirect_workflow/src/Controller/ModerationStateController.php
@@ -3,18 +3,46 @@
 namespace Drupal\nidirect_workflow\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class ModerationStateController.
  */
-class ModerationStateController extends ControllerBase {
+class ModerationStateController extends ControllerBase implements ContainerInjectionInterface {
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Creates a new ModerationStateConstraintValidator instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Change_state of specified entity.
    */
   public function changeState($nid, $new_state) {
     // Load the entity.
-    $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+    $entity = $this->entityTypeManager->getStorage('node')->load($nid);
     if ($entity) {
       // Request the state change.
       $entity->set('moderation_state', $new_state);

36546a1049fe8caf14d0b4500f868676dac91b15
DuttonMa <martin@theduttons.org>
2019-07-31 17:05:06 +0100

    coding standards

--- nidirect_common/tests/src/Kernel/DrivingInstructorTest.php
+++ nidirect_common/tests/src/Kernel/DrivingInstructorTest.php
@@ -5,7 +5,6 @@ namespace Drupal\Tests\nidirect_common\Kernel;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
-use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
 
@@ -30,8 +29,10 @@ class DrivingInstructorTest extends EntityKernelTestBase {
     parent::setUp();
 
     // Create a mock node type for testing.
-    NodeType::create(['type' => 'driving_instructor',
-      'label' => 'driving_instructor'])->save();
+    NodeType::create([
+      'type' => 'driving_instructor',
+      'label' => 'driving_instructor',
+    ])->save();
 
     // Add mocked fields.
     $fields = ['field_di_firstname', 'field_di_lastname', 'field_di_adi_no'];
@@ -62,11 +63,11 @@ class DrivingInstructorTest extends EntityKernelTestBase {
       'field_di_firstname' => [['value' => 'Firstname']],
       'field_di_lastname' => [['value' => 'Lastname']],
       'field_di_adi_no' => [['value' => '222']],
-      'uid' => $content_admin_user->id()
+      'uid' => $content_admin_user->id(),
     ]);
     $node->save();
+    // Title should have been automatically set to a combination of fields.
     $this->assertEquals('Firstname Lastname (ADI No. 222)', $node->getTitle());
-
   }
 
 }

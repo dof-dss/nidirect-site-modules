927ffeb42887000091b39628a809b83f38a33130
DuttonMa <martin@theduttons.org>
2019-08-05 16:47:30 +0100

    confirm route and controller added

--- nidirect_workflow/src/Controller/AuditController.php
+++ nidirect_workflow/src/Controller/AuditController.php
@@ -17,17 +17,35 @@ class AuditController extends ControllerBase {
    */
   public function contentAudit($nid) {
     $msg = 'Invalid node id';
+    // Don't forget translations.
+    // Don't forget dependency injection.
     if (!empty($nid)) {
       $node = \Drupal\node\Entity\Node::load($nid);
       if ($node) {
         $msg = "Click this button to indicate that you have audited this published content ";
         $msg .= "and are happy that it is still accurate and relevant.";
-        $msg .= "<div><a href='/nidirect_workflow/audit_confirm/$nid'>Audit this published content</a></div>";
+        $msg .= "<div><a href='/nidirect_workflow/confirm_audit/$nid'>Audit this published content</a></div>";
       }
     }
     return [
       '#type' => 'markup',
       '#markup' => $this->t($msg)
+    ];
+  }
+
+  /**
+   * Confirm Audit.
+   *
+   * @return string
+   *   Return confirmation string.
+   */
+  public function confirmAudit($nid) {
+    $msg = 'Invalid node id';
+    // Don't display anything, just do the update and
+    // follow the 'destination' param.
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Successfully audited')
     ];
   }
 

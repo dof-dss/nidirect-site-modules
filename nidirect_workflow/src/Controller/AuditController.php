746e226f77b4fc5cd864dfc129d6e4ceff515c4b
DuttonMa <martin@theduttons.org>
2019-08-05 16:39:34 +0100

    start audit confirmation page

--- nidirect_workflow/src/Controller/AuditController.php
+++ nidirect_workflow/src/Controller/AuditController.php
@@ -13,12 +13,21 @@ class AuditController extends ControllerBase {
    * Content Audit.
    *
    * @return string
-   *   Return cofirmation string.
+   *   Return confirmation string.
    */
-  public function contentAudit() {
+  public function contentAudit($nid) {
+    $msg = 'Invalid node id';
+    if (!empty($nid)) {
+      $node = \Drupal\node\Entity\Node::load($nid);
+      if ($node) {
+        $msg = "Click this button to indicate that you have audited this published content ";
+        $msg .= "and are happy that it is still accurate and relevant.";
+        $msg .= "<div><a href='/nidirect_workflow/audit_confirm/$nid'>Audit this published content</a></div>";
+      }
+    }
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('Do you want to audit ?')
+      '#markup' => $this->t($msg)
     ];
   }
 

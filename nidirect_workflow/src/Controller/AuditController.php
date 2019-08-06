b6487cb1588c2acb854655ff5cdbb19580fb9e43
DuttonMa <martin@theduttons.org>
2019-08-06 16:22:01 +0100

    confirm audit processing working

--- nidirect_workflow/src/Controller/AuditController.php
+++ nidirect_workflow/src/Controller/AuditController.php
@@ -4,6 +4,8 @@ namespace Drupal\nidirect_workflow\Controller;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
 use Drupal\Core\Controller\ControllerBase;
 use Psr\Log\LoggerInterface;
@@ -29,16 +31,26 @@ class AuditController extends ControllerBase implements ContainerInjectionInterf
   protected $logger;
 
   /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $account;
+
+  /**
    * Creates a new ModerationStateConstraintValidator instance.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Psr\Log\LoggerInterface $logger
    *   The logger interface.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The current user.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger, AccountInterface $account) {
     $this->entityTypeManager = $entity_type_manager;
     $this->logger = $logger;
+    $this->account = $account;
   }
 
   /**
@@ -47,7 +59,8 @@ class AuditController extends ControllerBase implements ContainerInjectionInterf
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
-      $container->get('logger.factory')->get('nidirect_workflow')
+      $container->get('logger.factory')->get('nidirect_workflow'),
+      $container->get('current_user')
     );
   }
 
@@ -63,12 +76,10 @@ class AuditController extends ControllerBase implements ContainerInjectionInterf
     $msg = 'Invalid node id';
     // Don't forget translations.
     if (!empty($nid)) {
-      //$node = Node::load($nid);
       $node = $this->entityTypeManager()->getStorage('node')->load($nid);
       if ($node) {
-        $msg = "Click this button to indicate that you have audited this published content ";
-        $msg .= "and are happy that it is still accurate and relevant.";
-        $msg .= "<div><a href='/nidirect_workflow/confirm_audit/$nid'>Audit this published content</a></div>";
+        $msg = t("Click this button to indicate that you have audited this published content and are happy that it is still accurate and relevant.");
+        $msg .= "<div><a href='/nidirect_workflow/confirm_audit/$nid'>" . t("Audit this published content") . "</a></div>";
       }
     }
     return [
@@ -84,13 +95,17 @@ class AuditController extends ControllerBase implements ContainerInjectionInterf
    *   Return confirmation string.
    */
   public function confirmAudit($nid) {
-    $msg = 'Invalid node id';
-    // Don't display anything, just do the update and
-    // follow the 'destination' param.
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Successfully audited'),
-    ];
+    // Bump up the 'next audit due' date and log it.
+    $node = $this->entityTypeManager()->getStorage('node')->load($nid);
+    if ($node) {
+      $node->set('field_next_audit_due',date('Y-m-d', strtotime("+6 months")));
+      $node->save();
+      $message = "nid " . $nid . " has been audited by " . $this->account->getAccountName() . " (uid " . $this->account->id() . ")";
+      $this->logger->notice($message);
+    }
+    // Redirect user to node view (although the 'destination'
+    // url argument will override this).
+    return $this->redirect('entity.node.canonical', ['node' => $nid]);
   }
 
 }

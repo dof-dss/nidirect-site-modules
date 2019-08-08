0befc5db7bac81849efc1178e6d6f2890b4afc7e
DuttonMa <martin@theduttons.org>
2019-08-08 14:42:58 +0100

    coding standards

--- nidirect_workflow/src/Form/AuditSettingsForm.php
+++ nidirect_workflow/src/Form/AuditSettingsForm.php
@@ -61,13 +61,6 @@ class AuditSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 

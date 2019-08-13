6b93b2feed1f85d3f336df9c03e25b72b8e43b83
Johan Gant <johan.gant@gmail.com>
2019-08-13 11:21:46 +0100

    Add a search breadcrumb handler class (#12)
    * Add a search breadcrumb handler class
    
    * Use service parameters to make the class more re-usable

---
+++ nidirect_breadcrumbs/src/SearchBreadcrumb.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\nidirect_breadcrumbs;
+
+/**
+ * @file
+ * Generates the breadcrumb trail for search page(s)
+ */
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class SearchBreadcrumb implements BreadcrumbBuilderInterface {
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(array $route_matches) {
+    $this->routeMatches = $route_matches;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('breadcrumb.search.matches')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $match = FALSE;
+
+    if (in_array($route_match->getRouteName(), $this->routeMatches)) {
+      $match = TRUE;
+    }
+
+    return $match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->setLinks([]);
+    $breadcrumb->addCacheContexts(['url.path']);
+
+    return $breadcrumb;
+  }
+
+}

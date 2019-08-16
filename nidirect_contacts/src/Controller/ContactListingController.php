c7d7702663caa1aeef54f9301f903e1d355189fe
Johan Gant <johan.gant@gmail.com>
2019-08-16 16:11:10 +0100

    Views query and exposed form preprocessing hooks, tweaks to routing too

--- nidirect_contacts/src/Controller/ContactListingController.php
+++ nidirect_contacts/src/Controller/ContactListingController.php
@@ -7,6 +7,7 @@ use Drupal\Core\Block\BlockManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\views\Views;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class ContactListingController.
@@ -28,11 +29,19 @@ class ContactListingController extends ControllerBase {
   protected $blockManager;
 
   /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $request;
+
+  /**
    * Constructs a new ContactListingController object.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, BlockManagerInterface $block_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, BlockManagerInterface $block_manager, RequestStack $request) {
     $this->entityTypeManager = $entity_type_manager;
     $this->blockManager = $block_manager;
+    $this->request = $request;
   }
 
   /**
@@ -41,7 +50,8 @@ class ContactListingController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
-      $container->get('plugin.manager.block')
+      $container->get('plugin.manager.block'),
+      $container->get('request_stack')
     );
   }
 
@@ -55,22 +65,54 @@ class ContactListingController extends ControllerBase {
    *    <Big A-Z, 0-9 grid of links>
    *    Link is: /contacts/letter/a
    *
-   * @return string
-   *   Return Hello string.
+   * @return array
+   *   Return render array for Drupal to convert to HTML.
    */
   public function default() {
     $content = [];
 
     // Views static service class used in absence of container service to inject via constructor.
-    $view = Views::getView('contacts');
     $content['contact_form'] = [
       '#type' => 'view',
       '#name' => 'contacts',
-      '#display_id' => 'search_form',
+      '#display_id' => 'contact_search',
     ];
 
     $az_block = $this->blockManager->createInstance('contact_az_block', []);
+    $content['contact_az_block'] = $az_block->build();
+
+    return $content;
+  }
+
+  /**
+   * A-Z contacts listing page.
+   *
+   * - Title: Contacts A to Z
+   * - Search by contact name
+   *    <input 'Search contacts' placeholder> <submit>
+   * - Find contacts beginning with...
+   *    <Big A-Z, 0-9 grid of links>
+   * - Reset a-z link | show search link
+   *
+   * @return array
+   *   Return render array for Drupal to convert to HTML.
+   */
+  public function filterByLetter(string $letter = NULL) {
+    // Trim letter parameter if, for whatever reason, it's > 1.
+    if (strlen($letter) > 1) {
+      $letter = substr($letter, 0, 1);
+    }
+
+
+    $az_block = $this->blockManager->createInstance('contact_az_block', []);
     $content['a-z'] = $az_block->build();
+
+    $content['contacts_a_z'] = [
+      '#type' => 'view',
+      '#name' => 'contacts_a_z',
+      '#display_id' => 'contacts_by_letter',
+      '#arguments' => [$letter],
+    ];
 
     return $content;
   }
--- nidirect_contacts/src/Plugin/Block/ContactAzBlock.php
+++ nidirect_contacts/src/Plugin/Block/ContactAzBlock.php
@@ -3,6 +3,11 @@
 namespace Drupal\nidirect_contacts\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'ContactAzBlock' block.
@@ -12,14 +17,68 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Contacts A to Z"),
  * )
  */
-class ContactAzBlock extends BlockBase {
+class ContactAzBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Routing\CurrentRouteMatch definition.
+   *
+   * @var Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $routeMatch;
+
+  /**
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param Drupal\Core\Routing\CurrentRouteMatch $route_match
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentRouteMatch $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    $build['contact_az_block']['#markup'] = 'Implement ContactAzBlock.';
+    $links = [];
+
+    $title = $this->t('Find contacts beginning with') . '...';
+
+    if ($this->routeMatch->getRouteName() == 'nidirect_contacts.letter') {
+      $letter = $this->routeMatch->getParameter('letter');
+      $title = $this->t('Showing entries for') . ' ' . strtoupper($letter);
+    }
+
+    $build['title'] = [
+      '#markup' => '<p>' . $title . '</p>',
+    ];
+
+    foreach (array_merge(range('a', 'z'), range('0', '9')) as $item) {
+      $links[] = Link::createFromRoute(strtoupper($item), 'nidirect_contacts.letter', ['letter' => $item], [
+        'attributes' => [
+          'title' => $this->t('View entries under') . ' ' . strtoupper($item)
+        ]
+      ])->toRenderable();
+    }
+
+    $build['contact_az_block'] = [
+      '#theme' => 'item_list',
+      '#items' => $links,
+    ];
 
     return $build;
   }

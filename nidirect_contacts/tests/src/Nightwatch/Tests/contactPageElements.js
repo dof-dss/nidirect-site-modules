284a365a5ec8b1b34248a74289713f81009b4856
Johan Gant <johan.gant@gmail.com>
2019-08-19 15:02:03 +0100

    Fix link bar rendering and add tests

--- nidirect_contacts/tests/src/Nightwatch/Tests/contactPageElements.js
+++ nidirect_contacts/tests/src/Nightwatch/Tests/contactPageElements.js
@@ -20,6 +20,51 @@ module.exports = {
       .drupalRelativeURL('/contacts/letter/a')
       .expect.element('.view-contacts-a-z nav.pager')
       .to.not.be.present;
-  }
+  },
+
+  'Contacts by letter confirms: "Showing entries for A"': browser => {
+    browser
+      .expect.element('.view-contacts-a-z .view-header')
+      .text.to.contain('Showing entries for A');
+  },
+
+  'Contacts by letter displays Reset search convenience link': browser => {
+    browser
+      .expect.element('.view-contacts-a-z .view-links')
+      .text.to.contain('Reset search');
+  },
+
+  'Contacts by letter displays Show A-Z convenience link': browser => {
+    browser
+      .expect.element('.view-contacts-a-z .view-links')
+      .text.to.contain('Show A-Z');
+  },
+
+  'Contacts by letter does not show any sorting links': browser => {
+    browser
+      .expect.element('.view-links')
+      .text.to.not.match(/Sort .+/);
+  },
+
+  'Contact landing page should not show any sorting or convenience links': browser => {
+    browser
+      .drupalRelativeURL('/contacts')
+      .expect.element('.view-links')
+      .to.not.be.present;
+  },
+
+  'Contact landing page should show the A-Z block': browser => {
+    browser
+      .drupalRelativeURL('/contacts')
+      .expect.element('#contacts-az')
+      .to.be.present;
+  },
+
+  'Contact landing page should show the A-Z block': browser => {
+    browser
+      .drupalRelativeURL('/contacts')
+      .expect.element('#contacts-az')
+      .to.be.present;
+  },
 
 };

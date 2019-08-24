269b515d7bd817346743ae906bb5259f4d6aaf05
Johan Gant <johan.gant@gmail.com>
2019-08-24 12:10:06 +0100

    Add module for social links and tools

---
+++ nidirect_sst/src/Plugin/Block/SharingLinks.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\nidirect_sst\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Provides a 'SharingLinks' block.
+ *
+ * @Block(
+ *  id = "sharing_links",
+ *  admin_label = @Translation("Sharing links"),
+ * )
+ */
+class SharingLinks extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    // Links for: Twitter, Facebook, Youtube and RSS.
+    $build['twitter'] = Link::fromTextAndUrl('Twitter',
+      Url::fromUri('https://twitter.com/nidirect', [
+        'attributes' => ['title' => 'Twitter']
+      ]))->toRenderable();
+    $build['fb'] = Link::fromTextAndUrl('Facebook',
+      Url::fromUri('https://www.facebook.com/nidirect', [
+        'attributes' => ['title' => 'Facebook']
+      ]))->toRenderable();
+    $build['youtube'] = Link::fromTextAndUrl('YouTube',
+      Url::fromUri('https://www.youtube.com/user/nidirect', [
+        'attributes' => ['title' => 'YouTube']
+      ]))->toRenderable();
+    // TODO: covert to route name once feed exists.
+    $build['rss'] = Link::fromTextAndUrl('RSS',
+      Url::fromUri('internal:/news-rss.xml', [
+        'attributes' => ['title' => 'RSS']
+      ]))->toRenderable();
+
+    return $build;
+  }
+
+}

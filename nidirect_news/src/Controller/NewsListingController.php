d7e0f7df278f2db6f281f03c5e1b350887ed8942
Johan Gant <johan.gant@gmail.com>
2019-09-05 13:16:40 +0100

    Custom module to provide news section

---
+++ nidirect_news/src/Controller/NewsListingController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\nidirect_news\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Block\BlockManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Class NewsListingController.
+ */
+class NewsListingController extends ControllerBase {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+  /**
+   * Drupal\Core\Block\BlockManagerInterface definition.
+   *
+   * @var \Drupal\Core\Block\BlockManagerInterface
+   */
+  protected $pluginManagerBlock;
+  /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * Constructs a new NewsListingController object.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, BlockManagerInterface $plugin_manager_block, RequestStack $request_stack) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->pluginManagerBlock = $plugin_manager_block;
+    $this->requestStack = $request_stack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('plugin.manager.block'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * Default page listing. Show:
+   *
+   * - Latest news: teaser view per item
+   * - Older news: title + date per item, with full pager.
+   *
+   * @return array
+   *   Render array for the page for Drupal to convert to HTML.
+   */
+  public function default() {
+    $content = [];
+
+    // Latest news embed display.
+    $content['latest_news'] = [
+      '#type' => 'view',
+      '#name' => 'news',
+      '#display_id' => 'latest_news',
+    ];
+
+    // Older news.
+    $content['older_news'] = [
+      '#type' => 'paragraph',
+      '#name' => 'news',
+      '#display_id' => 'older_news',
+    ];
+
+    return $content;
+  }
+
+}

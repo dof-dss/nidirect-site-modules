bf0480730ff7272d9fb68dcee98829796a3e0067
DuttonMa <martin@theduttons.org>
2019-10-18 15:43:23 +0100

    Add permission checking

--- nidirect_site_themes/src/Controller/SiteThemeController.php
+++ nidirect_site_themes/src/Controller/SiteThemeController.php
@@ -3,8 +3,12 @@
 namespace Drupal\nidirect_site_themes\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Link;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\taxonomy\Entity\Term;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class SiteThemeController.
@@ -29,7 +33,7 @@ class SiteThemeController extends ControllerBase {
    *
    */
   protected function printVocab($vid) {
-    $output = "<h1>Site Themes</h1><div class='item_list'><ul>";
+    $output = "<div class='item_list'><ul>";
     $terms = $this->entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, 0, 1);
     foreach ($terms as $term) {
       $output .= $this->printOneLevel($vid, $term->tid);
@@ -45,13 +49,19 @@ class SiteThemeController extends ControllerBase {
     $output = "<div class='item_list'><ul>";
     $term = Term::load($parent_tid);
     if (!empty($term)) {
-      $link_object = Link::createFromRoute(t('edit'), 'entity.taxonomy_term.edit_form', ['taxonomy_term' => $parent_tid]);
       $output .= "<li>";
+      // edit terms in site_themes
+      $account = $this->entityTypeManager()->getStorage('user')->load($this->currentUser()->id());
+      $edit_link = '';
+      if ($account->hasPermission('edit terms in site_themes')) {
+        $link_object = Link::createFromRoute(t('edit'), 'entity.taxonomy_term.edit_form', ['taxonomy_term' => $parent_tid]);
+        $edit_link = $link_object->toString();
+      }
       $output .= t(
             "@term (Topic ID: @tid) @edit_link", [
               '@term' => $term->getName(),
               '@tid' => $parent_tid,
-              '@edit_link' => $link_object->toString(),
+              '@edit_link' => $edit_link,
             ]
         );
       $output .= "</li>";

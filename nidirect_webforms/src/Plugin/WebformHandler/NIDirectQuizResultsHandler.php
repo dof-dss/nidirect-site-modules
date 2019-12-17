784982919b4a97aade0ff992464d30847b34fb36
omahm <hello@onemanandhismouse.com>
2019-12-17 17:09:19 +0000

    D8NID-169 ~ Initial Webform handler and config settings

---
+++ nidirect_webforms/src/Plugin/WebformHandler/NIDirectQuizResultsHandler.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Drupal\nidirect_webforms\Plugin\WebformHandler;
+
+use Drupal\Component\Utility\Xss;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Render\Markup;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformInterface;
+use Drupal\webform\WebformSubmissionConditionsValidatorInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * NIDirect Quiz Results Webform Handler.
+ *
+ * @WebformHandler(
+ *   id = "nidirect_quiz_results",
+ *   label = @Translation("Quiz Results"),
+ *   category = @Translation("NIDirect"),
+ *   description = @Translation("Displays quiz answers and feedback."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_IGNORED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED,
+ * )
+ */
+class NIDirectQuizResultsHandler extends WebformHandlerBase
+{
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    LoggerChannelFactoryInterface $logger_factory,
+    ConfigFactoryInterface $config_factory,
+    EntityTypeManagerInterface $entity_type_manager,
+    WebformSubmissionConditionsValidatorInterface $conditions_validator)
+  {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger_factory, $config_factory, $entity_type_manager, $conditions_validator);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory'),
+      $container->get('config.factory'),
+      $container->get('entity_type.manager'),
+      $container->get('webform_submission.conditions_validator')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'introduction' => '',
+      'pass_text' => '',
+      'fail_text' => '',
+      'pass_mark' => 0,
+      'feedback' => '',
+      'answers' => [],
+      'message' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+
+    $form['pass_mark'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Pass mark'),
+      '#value' => $this->configuration['pass_mark'],
+      '#description' => $this->t('Number of questions to get correct for a pass grade.'),
+      '#weight' => 1,
+    ];
+
+    // Result display.
+    $form['result'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Result text'),
+      '#weight' => 5,
+    ];
+
+    $form['result']['introduction'] = [
+      '#type' => 'webform_html_editor',
+      '#title' => $this->t('Introduction'),
+      '#format' => 'full_html',
+      '#value' => $this->configuration['introduction'],
+    ];
+
+    $form['result']['pass_text'] = [
+      '#type' => 'webform_html_editor',
+      '#title' => $this->t('Pass text'),
+      '#format' => 'full_html',
+      '#value' => $this->configuration['pass_text'],
+    ];
+
+    $form['result']['fail_text'] = [
+      '#type' => 'webform_html_editor',
+      '#title' => $this->t('Fail text'),
+      '#format' => 'full_html',
+      '#value' => $this->configuration['fail_text'],
+    ];
+
+    $form['result']['feedback'] = [
+      '#type' => 'webform_html_editor',
+      '#title' => $this->t('Feedback text'),
+      '#format' => 'full_html',
+      '#value' => $this->configuration['feedback'],
+    ];
+
+    return $form;
+  }
+
+}

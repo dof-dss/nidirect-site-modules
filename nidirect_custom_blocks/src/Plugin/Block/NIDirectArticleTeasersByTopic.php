bd90caa853c9b7436a77e1ce7fe88d918a947fb3
DuttonMa <martin@theduttons.org>
2019-12-18 09:48:01 +0000

    starting to render article teasers

--- nidirect_custom_blocks/src/Plugin/Block/NIDirectArticleTeasersByTopic.php
+++ nidirect_custom_blocks/src/Plugin/Block/NIDirectArticleTeasersByTopic.php
@@ -3,6 +3,7 @@
 namespace Drupal\nidirect_custom_blocks\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
 
 /**
  * Provides a 'NIDirectArticleTeasersByTopic' block.
@@ -19,9 +20,58 @@ class NIDirectArticleTeasersByTopic extends BlockBase {
    */
   public function build() {
     $build = [];
-    $build['nidirect_article_teasers_by_topic']['#markup'] = 'Implement NIDirectArticleTeasersByTopic.';
-
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $results = $this->renderArticleTeasersByTerm($node->field_subtheme->target_id);
+    $results += $this->renderArticleTeasersByTopic($node->field_subtheme->target_id);
+    //$build['nidirect_article_teasers_by_topic']['#markup'] = 'Implement NIDirectArticleTeasersByTopic.';
+    $build['nidirect_article_teasers_by_topic'] = $results;
     return $build;
+  }
+
+  /**
+   * Utility function to render 'articles by term' view.
+   */
+  private function renderArticleTeasersByTerm($tid) {
+    // Render the 'articles by term' view and process the results.
+    $results = [];
+    $articles_view = views_embed_view('articles_by_term', 'article_teasers_by_term_embed', $tid, $tid);
+    \Drupal::service('renderer')->renderRoot($articles_view);
+    foreach ($articles_view['view_build']['#view']->result as $row) {
+      // This will be a link to an article.
+      $results[strtolower($row->_entity->getTitle())] = [
+        '#type' => 'link',
+        '#title' => $row->_entity->getTitle(),
+        '#url' => Url::fromRoute('entity.node.canonical', ['node' => $row->nid]),
+      ];
+      // Add cache tag for each article.
+      $cache_tags[] = 'node:' . $row->nid;
+    }
+    return $results;
+  }
+
+  /**
+   * Utility function to render 'site subtopics' view.
+   */
+  private function renderArticleTeasersByTopic($tid) {
+    // Render the 'articles by term' view and process the results.
+    $results = [];
+    $articles_view = views_embed_view('site_subtopics', 'subtopic_teasers_by_topic_embed', $tid, $tid);
+    \Drupal::service('renderer')->renderRoot($articles_view);
+    foreach ($articles_view['view_build']['#view']->result as $row) {
+      // This will be a link to an article.
+      $title = $row->_entity->getName();
+      $summary = $row->_entity->field_theme_summary->value;
+      $tid = $row->tid;
+      $results[strtolower($row->_entity->getName())] = [
+        '#type' => 'link',
+        '#title' => $row->_entity->getName(),
+        '#url' => Url::fromRoute('entity.node.canonical', ['node' => $row->tid]),
+        '#suffix' => $summary
+      ];
+      // Add cache tag for each article.
+      $cache_tags[] = 'node:' . $row->nid;
+    }
+    return $results;
   }
 
 }

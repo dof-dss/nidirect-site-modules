45ff55d4721333a1ea6b7442d2e4590f066be343
DuttonMa <martin@theduttons.org>
2020-01-17 10:17:05 +0000

    js working

--- nidirect_custom_blocks/js/nidirect_sharing.js
+++ nidirect_custom_blocks/js/nidirect_sharing.js
@@ -1,8 +1,10 @@
-jQuery(document).ready(function() {
-  alert('hello');
-  jQuery('.facebook_share_link').click(function(e) {
-    e.preventDefault();
-    window.open($(this).attr('href'), 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
-    return false;
+(function ($) {
+  $(document).ready(function() {
+    alert('hello');
+    $('.facebook_share_link').click(function(e) {
+      e.preventDefault();
+      window.open($(this).attr('href'), 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+      return false;
+    });
   });
-});
+})(jQuery);

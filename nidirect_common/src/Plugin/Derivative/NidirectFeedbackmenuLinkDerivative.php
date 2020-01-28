7a42305d763e6f97020e9e757f33b34ee76544cd
DuttonMa <martin@theduttons.org>
2020-01-28 15:54:59 +0000

    coding standards

--- nidirect_common/src/Plugin/Derivative/NidirectFeedbackmenuLinkDerivative.php
+++ nidirect_common/src/Plugin/Derivative/NidirectFeedbackmenuLinkDerivative.php
@@ -1,15 +1,9 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\nidirect_common\Plugin\Derivative\NidirectFeedbackmenuLinkDerivative.php.
- */
 namespace Drupal\nidirect_common\Plugin\Derivative;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
-use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -17,30 +11,29 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @see \Drupal\nidirect_common\Plugin\Block\NidirectFeedbackmenuLinkDerivative
  */
-class NidirectFeedbackmenuLinkDerivative extends DeriverBase implements ContainerDeriverInterface
-{
+class NidirectFeedbackmenuLinkDerivative extends DeriverBase implements ContainerDeriverInterface {
+
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, $base_plugin_id)
-  {
+  public static function create(ContainerInterface $container, $base_plugin_id) {
     return new static();
   }
+
   /**
    * {@inheritdoc}
    */
-  public function getDerivativeDefinitions($base_plugin_definition)
-  {
-    $links = array();
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $links = [];
     $links['nidirect_feedbackmenulink'] = [
-        'title' => 'Feedback',
-        'menu_name' => 'main',
-        'route_name' => 'entity.webform.canonical',
-        'route_parameters' => [
-          'webform' => 'site_feedback',
-        ],
-        'weight' => 8
-      ] + $base_plugin_definition;
+      'title' => 'Feedback',
+      'menu_name' => 'main',
+      'route_name' => 'entity.webform.canonical',
+      'route_parameters' => [
+        'webform' => 'site_feedback',
+      ],
+      'weight' => 8,
+    ] + $base_plugin_definition;
     return $links;
   }
 

e587a7a56a4991d6dac48e4ee60b532307ff1ea1
DuttonMa <martin@theduttons.org>
2020-01-28 14:43:12 +0000

    feedback parameter working with caching sledgehammer

--- nidirect_common/src/Plugin/Menu/NidirectFeedbackMenu.php
+++ nidirect_common/src/Plugin/Menu/NidirectFeedbackMenu.php
@@ -16,21 +16,23 @@ use Drupal\Core\Menu\MenuLinkDefault;
  * Provides a default implementation for menu link plugins.
  */
 class NidirectFeedbackMenu extends MenuLinkDefault {
-  // Class overrides here.
   /**
    * {@inheritdoc}
    */
   public function getOptions() {
     $options = parent::getOptions();
     // Append the current path as 's' parameter.
-    $options['query']['s'] = Url::fromRoute('<current>')->toString();
+    $page = \Drupal::request()->getRequestUri();
+    if ($page == '/') {
+      $page = '/front';
+    }
+    $options['query']['s'] = $page;
     return $options;
   }
 
   /**
    * {@inheritdoc}
    *
-   * @todo Make cacheable once https://www.drupal.org/node/2582797 lands.
    */
   public function getCacheMaxAge() {
     return 0;

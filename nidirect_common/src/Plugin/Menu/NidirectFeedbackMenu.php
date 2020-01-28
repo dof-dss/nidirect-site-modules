19dba3892c68bbde398fc9be877d59d5e90e0ddb
DuttonMa <martin@theduttons.org>
2020-01-28 15:11:24 +0000

    Added DI

--- nidirect_common/src/Plugin/Menu/NidirectFeedbackMenu.php
+++ nidirect_common/src/Plugin/Menu/NidirectFeedbackMenu.php
@@ -7,16 +7,64 @@
 
 namespace Drupal\nidirect_common\Plugin\Menu;
 
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Lock\LockBackendInterface;
+use Drupal\Core\Menu\StaticMenuLinkOverridesInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
+use Drupal\locale\StringStorageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Menu\MenuLinkDefault;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Provides a default implementation for menu link plugins.
  */
 class NidirectFeedbackMenu extends MenuLinkDefault {
-  
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * Constructs a new LoginLogoutMenuLink.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Menu\StaticMenuLinkOverridesInterface $static_override
+   *   The static override storage.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StaticMenuLinkOverridesInterface $static_override, RequestStack $request_stack) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $static_override);
+
+    $this->requestStack = $request_stack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('menu_link.static.overrides'),
+      $container->get('request_stack')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -24,7 +72,8 @@ class NidirectFeedbackMenu extends MenuLinkDefault {
     $options = parent::getOptions();
 
     // Append the current path as 's' parameter.
-    $page = \Drupal::request()->getRequestUri();
+    //$page = \Drupal::request()->getRequestUri();
+    $page = $this->requestStack->getCurrentRequest()->getRequestUri();
     if ($page == '/') {
       // Homepage is a special case.
       $page = '/front';

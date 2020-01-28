d784433757f968a13cf33a2f9f983bd3fd52b316
Johan Gant <johan.gant@gmail.com>
2020-01-28 11:22:02 +0000

    Adjust service parameters and remove redundant code

--- nidirect_gp/src/Controller/GpSearchController.php
+++ nidirect_gp/src/Controller/GpSearchController.php
@@ -2,34 +2,19 @@
 
 namespace Drupal\nidirect_gp\Controller;
 
-use Drupal\Component\Plugin\PluginManagerInterface;
-use Drupal\Core\Block\BlockManagerInterface;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\geocoder\GeocoderInterface;
-use Drupal\geocoder\ProviderPluginManager;
 use Drupal\nidirect_gp\PostcodeExtractor;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class GpSearchController.
+ *
+ * GpSearchForm block class used to place exposed views filter into
+ * sidebar region without requiring the use of AJAX for exposed filters.
  */
 class GpSearchController extends ControllerBase {
-
-  /**
-   * Maximum distance (miles) for any proximity search to use.
-   *
-   * @var int
-   */
-  protected $proximityMaxDistance;
-
-  /**
-   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
 
   /**
    * Symfony\Component\HttpFoundation\RequestStack definition.
@@ -37,13 +22,6 @@ class GpSearchController extends ControllerBase {
    * @var \Symfony\Component\HttpFoundation\RequestStack
    */
   protected $requestStack;
-
-  /**
-   * Drupal\Core\Block\BlockManagerInterface definition.
-   *
-   * @var \Drupal\Core\Block\BlockManagerInterface
-   */
-  protected $pluginManagerBlock;
 
   /**
    * Postcode extractor service.
@@ -60,37 +38,45 @@ class GpSearchController extends ControllerBase {
   protected $geocoder;
 
   /**
-   * Geocoder provider service.
+   * Maximum distance (miles) for any proximity search to use.
    *
-   * @var Drupal\geocoder\ProviderPluginManager
+   * @var int
    */
-  protected $geocoderProvider;
+  protected $proximityMaxDistance;
+
+  /**
+   * Machine key of the geocoding service we want to use (sourced from container).
+   *
+   * @var string
+   */
+  protected $geocodingServiceId;
 
   /**
    * GpSearchController constructor.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   Entity type manager service.
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
-   *   Request stack.
-   * @param \Drupal\Core\Block\BlockManagerInterface $plugin_manager_block
-   *   Plugin manager interface.
+   *   Request stack object.
    * @param \Drupal\nidirect_gp\PostcodeExtractor $postcode_extractor
    *   Postcode extractor service.
-   * @param int $proximity_max_distance
-   *   Service parameter, in miles. Units set on views argument handler config.
    * @param \Drupal\geocoder\GeocoderInterface $geocoder
-   *   Geocoding service.
-   * @param \Drupal\geocoder\ProviderPluginManager $geocoder_provider
-   *   Geocoding provider manager.
+   *   Geocoder service.
+   * @param int $proximity_max_distance
+   *   Max distance in miles for geocoding radius.
+   * @param string $geocoding_service_id
+   *   Geocoding service ID.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, RequestStack $request_stack, BlockManagerInterface $plugin_manager_block, PostcodeExtractor $postcode_extractor, int $proximity_max_distance, GeocoderInterface $geocoder, ProviderPluginManager $geocoder_provider) {
-    $this->entityTypeManager = $entity_type_manager;
+  public function __construct(
+    RequestStack $request_stack,
+    PostcodeExtractor $postcode_extractor,
+    GeocoderInterface $geocoder,
+    int $proximity_max_distance,
+    string $geocoding_service_id
+  ) {
+
     $this->requestStack = $request_stack;
-    $this->pluginManagerBlock = $plugin_manager_block;
     $this->postcodeExtractor = $postcode_extractor;
-    $this->proximityMaxDistance = $proximity_max_distance;
     $this->geocoder = $geocoder;
-    $this->geocoderProvider = $geocoder_provider;
+    $this->proximityMaxDistance = $proximity_max_distance;
+    $this->geocodingServiceId = $geocoding_service_id;
   }
 
   /**
@@ -98,24 +84,25 @@ class GpSearchController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity_type.manager'),
       $container->get('request_stack'),
-      $container->get('plugin.manager.block'),
       $container->get('nidirect.postcode_extractor'),
-      $container->getParameter('nidirect_gp.proximity_max_distance'),
       $container->get('geocoder'),
-      $container->get('plugin.manager.geocoder.provider')
+      $container->getParameter('nidirect_gp.proximity_max_distance'),
+      $container->getParameter('nidirect_gp.geocoding_service')
     );
   }
 
   /**
    * Handles the request for GP practice content.
    *
+   * We could include the form render array from GpSearchForm.php for conciseness but by
+   * doing this we can't place the form in a block in the sidebar region as per the present design.
+   *
    * @return array
    *   Render array of items for Drupal to convert to a HTML response.
    */
   public function handleSearchRequest() {
-    // Query term is validated by the routing definition, see nidirect_gp.routing.yml.
+    // Query term is validated/handled by the routing definition, see nidirect_gp.routing.yml.
     $query_term = $this->requestStack->getCurrentRequest()->get('search_api_views_fulltext');
 
     $is_proximity_search = FALSE;
@@ -135,9 +122,10 @@ class GpSearchController extends ControllerBase {
       '#display_id' => $display_id,
     ];
 
+    // If it's a proximity search (detected postcode) then add arguments to the view.
     if ($view_id == 'gp_practices_proximity' && $is_proximity_search) {
       // Geocode the first postcode (only accepting single values in our search).
-      $geocode_coordinates = $this->geocoder->geocode($postcode[0], ['googlemaps'])->first()->getCoordinates();
+      $geocode_coordinates = $this->geocoder->geocode($postcode[0], [$this->geocodingServiceId])->first()->getCoordinates();
 
       // Pass the values into a single, pre-formatted string as per the argument handlers requirements:
       // Ie: LAT,LON[OPERATOR]MAX_DISTANCE.

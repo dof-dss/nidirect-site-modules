02025c40098a00058418d9c49860deb92dd44f00
DuttonMa <martin@theduttons.org>
2020-02-06 13:18:59 +0000

    javascript problem fixed

--- nidirect_landing_pages/js/landing_listing.js
+++ nidirect_landing_pages/js/landing_listing.js
@@ -6,22 +6,26 @@
 (function ($, Drupal) {
   Drupal.behaviors.landinglisting = {
     attach: (context) => {
-      // Add submit handler to file upload form.
-      $(context).find('.shs-select')
-        .on('change', function (event) {
-          var $form = $(this);
-          if ($(context).find('#edit-field-manually-control-listing-value').prop('checked') == true) {
-            // This is an edge case, user has already ordered the listings but then
-            // changes the subtheme. Just hide the fields until after the node is
-            // saved for now.
-            $(context).find('#edit-field-manually-control-listing-value')
-              .trigger('click');
-            $(context).find('#edit-field-manually-control-listing-wrapper')
-              .hide();
-            $(context).find('.field--type-entity-reference-revisions.field--name-field-listing.field--widget-paragraphs.js-form-wrapper.form-wrapper')
-              .hide();
-          }
-        });
+      // Look through all <select>s.
+      $(context).find('select').each(function() {
+        var $thisclass = $(this).attr('class');
+        // Find any whose class starts with 'shs-'.
+        if ($thisclass.indexOf('shs-') == 0) {
+          $(this).on("change", function(){
+            if ($(context).find('#edit-field-manually-control-listing-value').prop('checked') == true) {
+              // This is an edge case, user has already ordered the listings but then
+              // changes the subtheme. Just hide the fields until after the node is
+              // saved for now.
+              $(context).find('#edit-field-manually-control-listing-value')
+                .trigger('click');
+              $(context).find('#edit-field-manually-control-listing-wrapper')
+                .hide();
+              $(context).find('.field--type-entity-reference-revisions.field--name-field-listing.field--widget-paragraphs.js-form-wrapper.form-wrapper')
+                .hide();
+            }
+          });
+        }
+      });
     }
   };
 }(jQuery, Drupal));

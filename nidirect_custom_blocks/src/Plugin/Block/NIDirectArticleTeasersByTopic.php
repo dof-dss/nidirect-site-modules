bfaa882130af62bbcc95ecc5949acc91a38b0ee0
DuttonMa <martin@theduttons.org>
2020-02-07 15:28:58 +0000

    allow user ordering of teasers

--- nidirect_custom_blocks/src/Plugin/Block/NIDirectArticleTeasersByTopic.php
+++ nidirect_custom_blocks/src/Plugin/Block/NIDirectArticleTeasersByTopic.php
@@ -83,8 +83,17 @@ class NIDirectArticleTeasersByTopic extends BlockBase implements ContainerFactor
       }
       // Get a list of article teasers by topic.
       $results += $this->renderArticleTeasersByTopic($node->field_subtheme->target_id, $cache_tags);
-      // Sort entries alphabetically (regardless of type).
-      ksort($results);
+      if ($node->field_manually_control_listing->value) {
+        // Order has been set by the user.
+        $results = $this->userSortResults($node, $results);
+      } else {
+        // Sort entries alphabetically (regardless of type).
+        ksort($results);
+      }
+      // Remove 'key' that was used internally.
+      foreach ($results as  $key => $val) {
+        unset($results[$key]['key']);
+      }
       // Will be processed by block--nidirect-article-teasers-by-topic.html.twig.
       $build['nidirect_article_teasers_by_topic'] = $results;
       $build['nidirect_article_teasers_by_topic']['#cache'] = [
@@ -92,6 +101,29 @@ class NIDirectArticleTeasersByTopic extends BlockBase implements ContainerFactor
       ];
     }
     return $build;
+  }
+
+  /**
+   * Utility function to sort article teasers.
+   */
+  private function userSortResults($node, array $results) {
+    // Use the 'field_listing' paragraph field on the node
+    // to sort the teasers.
+    $new_results = [];
+    foreach ($node->field_listing->referencedEntities() as $para) {
+      if ($para->getType() == 'term_teaser') {
+        $id = 't:' . $para->get('field_term')->getValue()[0]['target_id'];
+      } else {
+        $id = 'a:' . $para->get('field_article')->getValue()[0]['target_id'];
+      }
+      foreach($results as $title => $this_result) {
+        if ($this_result['key'] == $id) {
+          $new_results[$title] = $this_result;
+          break;
+        }
+      }
+    }
+    return $new_results;
   }
 
   /**
@@ -120,6 +152,7 @@ class NIDirectArticleTeasersByTopic extends BlockBase implements ContainerFactor
         '#url' => Url::fromRoute('entity.node.canonical', ['node' => $row->nid]),
       ];
       $thisresult['summary_text'] = ['#markup' => $row->_entity->field_summary->value];
+      $thisresult['key'] = 'a:' . $row->nid;
       // Place in an array keyed by lower case title (for sorting).
       $results[strtolower($row->_entity->getTitle())] = $thisresult;
       // Add cache tag for each article.
@@ -150,6 +183,7 @@ class NIDirectArticleTeasersByTopic extends BlockBase implements ContainerFactor
         '#url' => Url::fromRoute('entity.taxonomy_term.canonical', ['taxonomy_term' => $row->tid]),
       ];
       $thisresult['summary_text'] = ['#markup' => $row->_entity->field_teaser->value];
+      $thisresult['key'] = 't:' . $row->tid;
       // Place in an array keyed by lower case title (for sorting).
       $results[strtolower($row->_entity->getName())] = $thisresult;
       // Add cache tag for each listed term.

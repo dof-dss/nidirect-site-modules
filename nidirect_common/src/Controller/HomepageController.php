8513389680892a6a5cf4767f1743b8ab871b9c3c
Johan Gant <johan.gant@gmail.com>
2020-04-01 15:05:20 +0100

    Define placeholder route for homepage
    Replaces potentially dangerous use of /node path and view display bound to that route

---
+++ nidirect_common/src/Controller/HomepageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\nidirect_common\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class HomepageController.
+ */
+class HomepageController extends ControllerBase {
+
+  /**
+   * Default callback.
+   *
+   * @return array
+   *   Return a render array.
+   */
+  public function default() {
+    return [];
+  }
+
+}

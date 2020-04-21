a518bdb177e8ddbc3fc34846960ada448bae6380
omahm <hello@onemanandhismouse.com>
2020-04-21 16:57:05 +0100

    D8NID-419 ~ Create map when location field is in the viewport

--- nidirect_contacts/js/gmaps-lazy-load.js
+++ nidirect_contacts/js/gmaps-lazy-load.js
@@ -6,8 +6,38 @@
 (function ($, Drupal) {
   Drupal.behaviors.gmapslazyLoad = {
     attach: (context) => {
+
+      let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+      };
+
+      let callback = (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              // Call google map creation.
+              entry.target.classList.add('gmap-loaded');
+
+              var mapLatLng = {
+                lat: parseFloat(entry.target.dataset.lat),
+                lng: parseFloat(entry.target.dataset.lng)
+              };
+
+              var map = new google.maps.Map(document.getElementById(entry.target.id), {
+                center: mapLatLng,
+                zoom: 12
+              });
+
+              observer.unobserve(entry.target);
+            }
+          });
+        };
+
       $(context).find('.gmap-lazy-load').each(function () {
-        console.log($(this));
+        let observer = new IntersectionObserver(callback, options);
+        let target = $(this)[0];
+        observer.observe(target);
       });
     }
   }

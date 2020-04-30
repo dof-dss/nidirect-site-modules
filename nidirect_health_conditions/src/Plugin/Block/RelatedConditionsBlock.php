87743eb654ce9a8cb3e07fcddb0438784038d808
omahm <hello@onemanandhismouse.com>
2020-04-30 15:25:35 +0100

    D8NID-629 ~ Return empty render array if no conditions found

--- nidirect_health_conditions/src/Plugin/Block/RelatedConditionsBlock.php
+++ nidirect_health_conditions/src/Plugin/Block/RelatedConditionsBlock.php
@@ -126,6 +126,11 @@ class RelatedConditionsBlock extends BlockBase implements ContainerFactoryPlugin
         }
       }
 
+      // Prevent the block title from displaying if we don't have any links.
+      if (empty($links)) {
+        return [];
+      }
+
       $content['related_conditions'] = [
         '#theme' => 'item_list',
         '#items' => $links,

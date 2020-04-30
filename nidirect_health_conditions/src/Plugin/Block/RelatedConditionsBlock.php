5a0f5dcd13ec004f557f20b04e42898420438392
omahm <hello@onemanandhismouse.com>
2020-04-30 15:33:28 +0100

    D8NID-629 ~ Include cache context to prevent empty result caching

--- nidirect_health_conditions/src/Plugin/Block/RelatedConditionsBlock.php
+++ nidirect_health_conditions/src/Plugin/Block/RelatedConditionsBlock.php
@@ -128,7 +128,13 @@ class RelatedConditionsBlock extends BlockBase implements ContainerFactoryPlugin
 
       // Prevent the block title from displaying if we don't have any links.
       if (empty($links)) {
-        return [];
+        return [
+          '#cache' => [
+            'contexts' => [
+              'url.path',
+            ]
+          ],
+        ];
       }
 
       $content['related_conditions'] = [

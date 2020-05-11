a124d69ffbdf31207c5af758b3c7780bf0e1e0e8
Johan Gant <johan.gant@gmail.com>
2020-05-11 14:07:04 +0100

    Update deprecated service reference

--- nidirect_gp/src/Form/GpRevisionRevertTranslationForm.php
+++ nidirect_gp/src/Form/GpRevisionRevertTranslationForm.php
@@ -51,7 +51,7 @@ class GpRevisionRevertTranslationForm extends GpRevisionRevertForm {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity.manager')->getStorage('gp'),
+      $container->get('entity_type.manager')->getStorage('gp'),
       $container->get('date.formatter'),
       $container->get('language_manager')
     );

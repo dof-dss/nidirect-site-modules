5ec9f6046f5af3d22674da41ab056fb158095902
omahm <hello@onemanandhismouse.com>
2020-05-20 16:45:27 +0100

    D8NID-680 ~ Detect geolocation support and prepend markup to form

--- nidirect_gp/js/gp-location-search.js
+++ nidirect_gp/js/gp-location-search.js
@@ -7,6 +7,15 @@
   Drupal.behaviors.gpLocationSearch = {
     attach: (context) => {
 
+      $(context).find('#views-exposed-form-gp-practices-find-a-gp').once('gp-location-search').each(function () {
+        // Detect if the the browser supports the Geolocation API
+        if('geolocation' in navigator) {
+          $(this).prepend('<div>' +
+            '<label for="use_location">Search for a GP practice near you</label>' +
+            '<input type="button" id="use_location" name="use_location" value="Use my location" />' +
+            '</div>');
+        }
+      });
     }
   }
 }(jQuery, Drupal));

1b68b3f9c8da3dd27f2ad54d16b92ecba4e4669f
omahm <hello@onemanandhismouse.com>
2020-05-20 16:28:12 +0100

    D8NID-680 ~ Adding JS library fro geolocation scripts

---
+++ nidirect_gp/js/gp-location-search.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Defines Javascript behaviors for Google Maps Lazy Load Formatter.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.gpLocationSearch = {
+    attach: (context) => {
+
+    }
+  }
+}(jQuery, Drupal));

52a29b02030f5851641b5f7f1901402f410ceeaf
omahm <hello@onemanandhismouse.com>
2020-05-20 17:01:35 +0100

    D8NID-680 ~ Location request, Success and fail callbacks

--- nidirect_gp/js/gp-location-search.js
+++ nidirect_gp/js/gp-location-search.js
@@ -7,6 +7,16 @@
   Drupal.behaviors.gpLocationSearch = {
     attach: (context) => {
 
+      // Callback function to initiate the GP location search.
+      function performLocationSearch(lat, lng) {
+        console.log(lat, lng);
+      }
+
+      // Callback function for geolocation errors.
+      function locationError() {
+        console.log('unable to fetch your current location');
+      }
+
       $(context).find('#views-exposed-form-gp-practices-find-a-gp').once('gp-location-search').each(function () {
         // Detect if the the browser supports the Geolocation API
         if('geolocation' in navigator) {
@@ -14,8 +24,13 @@
             '<label for="use_location">Search for a GP practice near you</label>' +
             '<input type="button" id="use_location" name="use_location" value="Use my location" />' +
             '</div>');
+
+          $(this).find('#use_location').on('click', function() {
+            navigator.geolocation.getCurrentPosition(performLocationSearch, locationError);
+          })
         }
       });
     }
-  }
+  };
+
 }(jQuery, Drupal));

cb802e43154a16d1c02822a459b07dfa9d2a1cca
Glugmeister <neil.blair@finance-ni.gov.uk>
2020-06-01 20:44:23 +0100

    Display drupal's throbber when a location search is initiated. If location search fails: throbber is removed, drupal dialog is displayed with error, focus is set on the normal search input when the dialog is closed.

--- nidirect_gp/js/gp-location-search.js
+++ nidirect_gp/js/gp-location-search.js
@@ -7,6 +7,12 @@
   Drupal.behaviors.gpLocationSearch = {
     attach: (context) => {
 
+      let locationOptions = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
       // Callback function to initiate the GP location search.
       function performLocationSearch(location) {
         let lat = location.coords.latitude;
@@ -18,8 +24,16 @@
       }
 
       // Callback function for geolocation errors.
-      function locationError() {
-        const confirmationDialog = Drupal.dialog('<div>Unable to determine your location.</div>', {
+      function locationError(err) {
+
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        $('#find-by-location-status').html('');
+
+        let errmsg = '' +
+          '<p>There was a problem finding your location.</p>' +
+          '<p>Try searching by entering a GP name, practice, town or postcode</p>';
+
+        const confirmationDialog = Drupal.dialog('<div>' + errmsg + '.</div>', {
           title: Drupal.t('Sorry'),
           dialogClass: 'editor-change-text-format-modal',
           resizable: false,
@@ -29,6 +43,7 @@
             class: 'button button--primary',
             click() {
               confirmationDialog.close();
+              $('#edit-search-api-views-fulltext').focus();
             },
           }]
         });
@@ -37,16 +52,27 @@
       }
 
       $(context).find('#views-exposed-form-gp-practices-find-a-gp').once('gp-location-search').each(function () {
+        // Add accessible container for showing status messages during a location search
+        //$(this).after('<div id="find-by-location-status" role="alert" aria-live="assertive"></div>');
+
         // Detect if the the browser supports the Geolocation API
         if('geolocation' in navigator) {
-          $(this).prepend('<div>' +
+          $(this).prepend('<div class="find-by-location">' +
             '<label for="use_location">Search for a GP practice near you</label>' +
             '<input type="button" id="use_location" name="use_location" value="Use my location" />' +
+            '<div id="find-by-location-status" role="alert" aria-live="assertive"></div>' +
             '</div>');
 
-          $(this).find('#use_location').on('click', function() {
-            navigator.geolocation.getCurrentPosition(performLocationSearch, locationError);
-          })
+          $(this)
+            .find('#use_location').on('click', function() {
+              // Update status with a ajax progress indicator (even though it's not an ajax call).
+              $('#find-by-location-status').html(Drupal.theme.ajaxProgressIndicatorFullscreen());
+
+              // Do the location search
+              navigator.geolocation.getCurrentPosition(performLocationSearch, locationError, locationOptions);
+            })
+            .find('label[for="edit-search-api-views-fulltext"]')
+            .text('Or enter a GP name, practice, town or postcode');
         }
       });
     }

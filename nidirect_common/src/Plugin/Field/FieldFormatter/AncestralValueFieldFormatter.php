1ba3fc532f39efa995f70a15381207859086b45f
omahm <hello@onemanandhismouse.com>
2020-06-26 15:10:42 +0100

    D8NID-742 ~ Load ancestor terms and fetch field items

--- nidirect_common/src/Plugin/Field/FieldFormatter/AncestralValueFieldFormatter.php
+++ nidirect_common/src/Plugin/Field/FieldFormatter/AncestralValueFieldFormatter.php
@@ -2,9 +2,13 @@
 
 namespace Drupal\nidirect_common\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Field formatter "AncestralValueFieldFormatter".
@@ -19,7 +23,38 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class AncestralValueFieldFormatter extends FormatterBase {
+class AncestralValueFieldFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Create an instance of ContentModerationStateFormatter.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -58,17 +93,33 @@ class AncestralValueFieldFormatter extends FormatterBase {
 
       if ($entity->hasField('field_subtheme') && !$entity->get('field_subtheme')->isEmpty()) {
         $term = $entity->get('field_subtheme')->entity;
-        // todo: try and load the field value from the term ancestors.
+
+        // This issue https://www.drupal.org/node/2019905
+        // prevents us from using ->loadParents() as we won't
+        // retrieve the root term.
+        $ancestors = array_values($this->entityTypeManager->getStorage('taxonomy_term')->loadAllParents($term->id()));
+
+        // Remove the current term from the list of ancestors.
+        array_shift($ancestors);
+
+        foreach ($ancestors as $ancestor) {
+          $field = $ancestor->get('field_additional_info');
+          $items = $field->getIterator();
+
+          if ($items->count()) {
+            break;
+          }
+        }
       }
-    } else {
-      foreach ($items as $delta => $item) {
-        $elements[$delta] = [
-          '#type' => 'processed_text',
-          '#text' => $item->value,
-          '#format' => $item->format,
-          '#langcode' => $item->getLangcode(),
-        ];
-      }
+    }
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#type' => 'processed_text',
+        '#text' => $item->value,
+        '#format' => $item->format,
+        '#langcode' => $item->getLangcode(),
+      ];
     }
 
     return $elements;

d53ca549064834ef9eecbd5829cf04ae178df92f
omahm <hello@onemanandhismouse.com>
2020-06-26 14:04:54 +0100

    D8NID-742 ~ Initial Ancestral formatter

---
+++ nidirect_common/src/Plugin/Field/FieldFormatter/AncestralValueFieldFormatter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\nidirect_common\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Field formatter "AncestralValueFieldFormatter".
+ *
+ * @FieldFormatter(
+ *   id = "ancestral_value_field_formatter",
+ *   label = @Translation("Ancestral value formatter"),
+ *   description = @Translation("Fetch value from the current field, parent or grandparent."),
+ *   field_types = {
+ *     "text",
+ *     "text_long",
+ *   }
+ * )
+ */
+class AncestralValueFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#type' => 'processed_text',
+        '#text' => $item->value,
+        '#format' => $item->format,
+        '#langcode' => $item->getLangcode(),
+      ];
+    }
+
+    return $elements;
+  }
+
+}

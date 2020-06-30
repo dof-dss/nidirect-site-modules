b53a96a75ab41e6289874230488ad21bd3b79969
Johan Gant <johan.gant@gmail.com>
2020-06-30 16:47:43 +0100

    Rework featured content block plugin to use tag based selection

--- nidirect_homepage/src/Plugin/Block/FeaturedContentBlock.php
+++ nidirect_homepage/src/Plugin/Block/FeaturedContentBlock.php
@@ -3,17 +3,13 @@
 namespace Drupal\nidirect_homepage\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\taxonomy\Entity\Term;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a block to show a single featured content list.
- *
- * TODO: This class will need refining to be more selective over which
- * featured_content_list node it renders. At present, only the homepage will
- * use an instance of this node type but if the same are needed across other
- * site locations, a taxonomy driven approach will likely be required to organise
- * and select them by.
  *
  * @Block(
  *  id = "featured_content",
@@ -23,19 +19,39 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class FeaturedContentBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Drupal\Core\Entity\EntityManagerInterface definition.
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
    *
-   * @var \Drupal\Core\Entity\EntityManagerInterface
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $entityManager;
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new featured content block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = new static($configuration, $plugin_id, $plugin_definition);
-    $instance->entityManager = $container->get('entity.manager');
-    return $instance;
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
   }
 
   /**
@@ -44,16 +60,25 @@ class FeaturedContentBlock extends BlockBase implements ContainerFactoryPluginIn
   public function build() {
     $build = [];
 
-    // Load the first featured content list node.
-    $fcl_nodes = $this->entityManager->getStorage('node')->loadByProperties([
-      'type' => 'featured_content_list'
-    ]);
+    // Our featured content list will be tagged with the "Homepage" term, so load that term object.
+    $homepage_tag = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->loadByProperties(['name' => 'Homepage']);
+    $homepage_tag = reset($homepage_tag);
 
-    if (!empty($fcl_nodes)) {
-      $node_render = $this->entityManager->getViewBuilder('node')->view(reset($fcl_nodes));
+    if ($homepage_tag instanceof Term) {
+      // Load the first featured content list node.
+      $fcl_nodes = $this->entityTypeManager->getStorage('node')->loadByProperties([
+        'type' => 'featured_content_list',
+        'field_tags' => $homepage_tag->id(),
+      ]);
 
-      $build['#theme'] = 'featured_content';
-      $build['featured_content'] = $node_render;
+      if (!empty($fcl_nodes)) {
+        $node_render = $this->entityTypeManager->getViewBuilder('node')->view(reset($fcl_nodes));
+
+        $build['#theme'] = 'featured_content';
+        $build['featured_content'] = $node_render;
+      }
     }
 
     return $build;

681c884e7c38da2b9d980142354a7da2a4a8a896
DuttonMa <martin@theduttons.org>
2020-07-22 14:40:20 +0100

    test refactored, common tests from origins removed, only NIDirect specific tests left

--- nidirect_common/tests/src/Nightwatch/Tests/EditorTest.js
+++ nidirect_common/tests/src/Nightwatch/Tests/EditorTest.js
@@ -1,14 +1,9 @@
 module.exports = {
-  '@tags': ['origins', 'origins_workflow'],
+  '@tags': ['nidirect'],
 
   'Test login': browser => {
     browser
       .drupalLogin({ name: process.env.NW_TEST_USER_PREFIX + '_editor', password: process.env.TEST_PASS });
-
-    browser
-      .drupalRelativeURL('/node/add')
-      .expect.element('h1.page-title')
-      .text.to.contain('Add content');
 
     browser
       .drupalRelativeURL('/gp/add')
@@ -106,44 +101,9 @@ module.exports = {
       .text.to.not.contain('Webform');
 
     browser
-      .drupalRelativeURL('/admin/content')
-      .expect.element('h1.page-title')
-      .text.to.contain('Content');
-
-    browser
       .drupalRelativeURL('/admin/content/scheduled')
       .expect.element('h1.page-title')
       .text.to.contain('Scheduled Content');
-
-    browser
-      .drupalRelativeURL('/admin/content/media')
-      .expect.element('h1.page-title')
-      .text.to.contain('Media');
-
-    browser
-      .drupalRelativeURL('/admin/content/media')
-      .expect.element('div.region-content')
-      .text.to.contain('Add media');
-
-    browser
-      .drupalRelativeURL('/admin/content/drafts')
-      .expect.element('h1.page-title')
-      .text.to.contain('My Drafts');
-
-    browser
-      .drupalRelativeURL('/admin/content/all-drafts')
-      .expect.element('h1.page-title')
-      .text.to.contain('All drafts');
-
-    browser
-      .drupalRelativeURL('/admin/content/needs-review')
-      .expect.element('h1.page-title')
-      .text.to.contain('Needs Review');
-
-    browser
-      .drupalRelativeURL('/admin/content/needs-audit')
-      .expect.element('h1.page-title')
-      .text.to.contain('Needs Audit');
 
   }
 

2aaa764940975a351be0d86931fe28f31bb2033b
DuttonMa <martin@theduttons.org>
2020-07-22 13:55:48 +0100

    test refactored, common tests from origins removed, only NIDirect specific tests left

--- nidirect_common/tests/src/Nightwatch/Tests/AuthenticatedTest.js
+++ nidirect_common/tests/src/Nightwatch/Tests/AuthenticatedTest.js
@@ -6,47 +6,12 @@ module.exports = {
       .drupalLogin({ name: process.env.NW_TEST_USER_PREFIX + '_authenticated', password: process.env.TEST_PASS });
 
     browser
-      .drupalRelativeURL('/node/add')
-      .expect.element('h1.page-title')
-      .text.to.contain('Access denied');
-
-    browser
       .drupalRelativeURL('/gp/add')
       .expect.element('h1.page-title')
       .text.to.contain('Access denied');
 
     browser
-      .drupalRelativeURL('/admin/content')
-      .expect.element('h1.page-title')
-      .text.to.contain('Content');
-
-    browser
       .drupalRelativeURL('/admin/content/scheduled')
-      .expect.element('h1.page-title')
-      .text.to.contain('Access denied');
-
-    browser
-      .drupalRelativeURL('/admin/content/media')
-      .expect.element('h1.page-title')
-      .text.to.contain('Access denied');
-
-    browser
-      .drupalRelativeURL('/admin/content/drafts')
-      .expect.element('h1.page-title')
-      .text.to.contain('Access denied');
-
-    browser
-      .drupalRelativeURL('/admin/content/all-drafts')
-      .expect.element('h1.page-title')
-      .text.to.contain('Access denied');
-
-    browser
-      .drupalRelativeURL('/admin/content/needs-review')
-      .expect.element('h1.page-title')
-      .text.to.contain('Access denied');
-
-    browser
-      .drupalRelativeURL('/admin/content/needs-audit')
       .expect.element('h1.page-title')
       .text.to.contain('Access denied');
 

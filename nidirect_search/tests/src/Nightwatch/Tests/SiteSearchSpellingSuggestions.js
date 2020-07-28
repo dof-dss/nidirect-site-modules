00e96e20f7cdb75a92c84f9d28a0c5c835b809dd
Johan Gant <johan.gant@gmail.com>
2020-07-28 10:05:03 +0100

    Fixed tests for search counts + spelling suggestions

--- nidirect_search/tests/src/Nightwatch/Tests/SiteSearchSpellingSuggestions.js
+++ nidirect_search/tests/src/Nightwatch/Tests/SiteSearchSpellingSuggestions.js
@@ -5,23 +5,47 @@ module.exports = {
 
     browser
       .drupalRelativeURL('/search?query=covod')
-      .expect.element('.sapi-did-you-mean')
-      .text.to.contain('Did you mean: covid');
+      .expect.element('.sapi-did-you-mean').text.to.contain('Did you mean');
+    browser.expect.element('.sapi-did-you-mean > a').text.to.contain('covid');
 
     browser
       .drupalRelativeURL('/search?query=benfit')
-      .expect.element('.sapi-did-you-mean')
-      .text.to.contain('Did you mean: benefit');
+      .expect.element('.sapi-did-you-mean').text.to.contain('Did you mean');
+    browser.expect.element('.sapi-did-you-mean > a').text.to.contain('benefit');
 
     browser
       .drupalRelativeURL('/search?query=medcal')
-      .expect.element('.sapi-did-you-mean')
-      .text.to.contain('Did you mean: medical');
+      .expect.element('.sapi-did-you-mean').text.to.contain('Did you mean');
+    browser.expect.element('.sapi-did-you-mean > a').text.to.contain('medical');
 
     browser
       .drupalRelativeURL('/search?query=belfst')
-      .expect.element('.sapi-did-you-mean')
-      .text.to.contain('Did you mean: belfast');
+      .expect.element('.sapi-did-you-mean').text.to.contain('Did you mean');
+    browser.expect.element('.sapi-did-you-mean > a').text.to.contain('belfast');
+
+  },
+
+  'Confirm search results on: site search, contacts search, GP search, & health conditions search pages': browser => {
+
+    // Site search.
+    browser
+      .drupalRelativeURL('/search?query=nidirect')
+      .expect.element('h2.view--count').to.be.present;
+
+    // Contacts search.
+    browser
+      .drupalRelativeURL('/contacts?query_contacts_az=services')
+      .expect.element('h2.view--count').to.be.present;
+
+    // GP search.
+    browser
+      .drupalRelativeURL('/services/gp-practices?search_api_views_fulltext=surgery')
+      .expect.element('h2.view--count').to.be.present;
+
+    // Health conditions: search term (A-Z uses db search).
+    browser
+      .drupalRelativeURL('/services/health-conditions-a-z?query_health_az=cough')
+      .expect.element('.view-display-id-search_page .view-empty').not.to.be.present;
 
   }
 

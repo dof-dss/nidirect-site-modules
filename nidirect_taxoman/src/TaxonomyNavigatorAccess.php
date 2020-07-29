a9db60dd1296e2c3845df67db297485a70c5f186
omahm <hello@onemanandhismouse.com>
2020-07-29 11:34:38 +0100

    D8NID-806 ~ Move access control to static methods and support fallback to core permissions

--- nidirect_taxoman/src/TaxonomyNavigatorAccess.php
+++ nidirect_taxoman/src/TaxonomyNavigatorAccess.php
@@ -4,20 +4,50 @@ namespace Drupal\nidirect_taxoman;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\Access\AccessInterface;
-use Drupal\Core\Session\AccountProxy;
 
 /**
  * Provides additional permissions for entities provided by Taxonomy module.
  */
 class TaxonomyNavigatorAccess implements AccessInterface {
 
-  public function canViewVocabularyTerms($vocabulary, $taxonomy_term, AccountProxy $account) {
-
-    $moduleHandler = \Drupal::service('module_handler');
-    if ($moduleHandler->moduleExists('taxonomy_access_fix')){
-      return AccessResult::allowedIf($account->hasPermission('view terms in ' . $vocabulary));
-    }
-    return AccessResult::allowed();
+  public static function canViewTerms($vocabulary) {
+    return self::taxonomyAccess('view', $vocabulary);
   }
 
+  public static function canEditTerms($vocabulary) {
+    return self::taxonomyAccess('edit', $vocabulary);
+  }
+
+  public static function canDeleteTerms($vocabulary) {
+    return self::taxonomyAccess('delete', $vocabulary);
+  }
+
+  public static function canReorderTerms($vocabulary) {
+    return self::taxonomyAccess('reorder', $vocabulary);
+  }
+
+  public static function taxonomyAccess($type, $vocabulary) {
+    $moduleHandler = \Drupal::service('module_handler');
+    $taf_enabled = $moduleHandler->moduleExists('taxonomy_access_fix');
+
+    switch ($type) {
+      case 'view':
+        $permission_query = ($taf_enabled) ? 'view terms in ' . $vocabulary : 'Access the taxonomy vocabulary overview page';
+        break;
+      case 'edit':
+      case 'reorder':
+        $permission_query = ($taf_enabled) ? 'edit terms in ' . $vocabulary : $vocabulary . ': Edit terms';
+        break;
+      case 'delete':
+        $permission_query = ($taf_enabled) ? 'delete terms in ' . $vocabulary : $vocabulary . ': Delete terms';
+        break;
+    }
+
+    if (!empty($permission_query)) {
+      $result = \Drupal::currentUser()->hasPermission($permission_query);
+      return $result ? AccessResult::allowed() : AccessResult::forbidden();
+    }
+
+    return AccessResult::forbidden();
+  }
 }

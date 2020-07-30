64675fc8bfee94f5b697b9ef1404c8baa2aa8ea4
omahm <hello@onemanandhismouse.com>
2020-07-30 09:54:14 +0100

    D8NID-788 ~ Remove redundant validation method

--- nidirect_taxonomy_navigator/src/Form/SetParentTermForm.php
+++ nidirect_taxonomy_navigator/src/Form/SetParentTermForm.php
@@ -147,13 +147,6 @@ class SetParentTermForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $tid = $this->getRouteMatch()->getParameter('taxonomy_term');
     $form_values = $form_state->getValues();

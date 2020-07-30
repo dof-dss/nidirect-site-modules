e834ed64cfd494034121d510db9921530188d913
omahm <hello@onemanandhismouse.com>
2020-07-30 11:54:53 +0100

    D8NID-788 ~ Remove redundant validation method

--- nidirect_taxonomy_navigator/src/Form/TaxonomyNavigatorForm.php
+++ nidirect_taxonomy_navigator/src/Form/TaxonomyNavigatorForm.php
@@ -220,16 +220,6 @@ class TaxonomyNavigatorForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    foreach ($form_state->getValues() as $key => $value) {
-      // @TODO: Validate fields.
-    }
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
     $form_values = $form_state->getValues();

80fa35b2344329cb685ed988dbaf97c36765c959
omahm <hello@onemanandhismouse.com>
2020-08-21 11:42:14 +0100

    D8NID-830 ~ Initial campaign pages listings

--- nidirect_campaign_utilities/src/Controller/NidirectCampaignListController.php
+++ nidirect_campaign_utilities/src/Controller/NidirectCampaignListController.php
@@ -3,6 +3,7 @@
 namespace Drupal\nidirect_campaign_utilities\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
 
 /**
  * Returns responses for NIDirect Campaign Utilities routes.
@@ -14,9 +15,20 @@ class NidirectCampaignListController extends ControllerBase {
    */
   public function build() {
 
+    $conn_drupal7 = Database::getConnection('default', 'migrate');
+
+    $query = $conn_drupal7->query(
+      "SELECT nid, title, status FROM {node} WHERE type = 'landing_page'");
+    $d7_landing_pages = $query->fetchAll();
+
+    foreach ($d7_landing_pages as $landing_page) {
+      $items[] = $landing_page->nid . ' | ' . $landing_page->title . ' | ' . $landing_page->status;
+    }
+
+
     $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#theme' => 'item_list',
+      '#items' => $items,
     ];
 
     return $build;

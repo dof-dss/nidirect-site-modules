d08ccfe00ad5a27c85365582f11a17f720a64fe7
omahm <hello@onemanandhismouse.com>
2020-08-21 14:12:12 +0100

    D8NID-830 ~ Display campaigns as table

--- nidirect_campaign_utilities/src/Controller/NidirectCampaignListController.php
+++ nidirect_campaign_utilities/src/Controller/NidirectCampaignListController.php
@@ -4,6 +4,7 @@ namespace Drupal\nidirect_campaign_utilities\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Database;
+use Drupal\Core\Url;
 
 /**
  * Returns responses for NIDirect Campaign Utilities routes.
@@ -17,19 +18,26 @@ class NidirectCampaignListController extends ControllerBase {
 
     $conn_drupal7 = Database::getConnection('default', 'migrate');
 
-    $query = $conn_drupal7->query(
-      "SELECT nid, title, status FROM {node} WHERE type = 'landing_page'");
+    $query = $conn_drupal7->query("SELECT nid, title, status FROM {node} WHERE type = 'landing_page' ORDER BY title");
     $d7_landing_pages = $query->fetchAll();
 
+    $items = [];
+
     foreach ($d7_landing_pages as $landing_page) {
-      $items[] = $landing_page->nid . ' | ' . $landing_page->title . ' | ' . $landing_page->status;
+      $items[] = ['nid' => $landing_page->nid, 'title' => $landing_page->title, 'Published' => $landing_page->status ? 'yes' : 'no'];
     }
 
-
     $build['content'] = [
-      '#theme' => 'item_list',
-      '#items' => $items,
+      '#type' => 'table',
+      '#header' => [
+        $this->t('NID'),
+        $this->t('Title'),
+        $this->t('Published'),
+      ],
+      '#empty' => $this->t('No campaign content found.'),
     ];
+
+    $build['content']['#rows'] = $items;
 
     return $build;
   }

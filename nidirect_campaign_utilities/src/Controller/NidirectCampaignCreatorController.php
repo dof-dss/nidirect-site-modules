dda9b2d8e6006809b735d7365a513d5943bf9518
omahm <hello@onemanandhismouse.com>
2020-08-23 11:22:00 +0100

    D8NID-830 ~ Initial D7 content parsing

--- nidirect_campaign_utilities/src/Controller/NidirectCampaignCreatorController.php
+++ nidirect_campaign_utilities/src/Controller/NidirectCampaignCreatorController.php
@@ -2,8 +2,14 @@
 
 namespace Drupal\nidirect_campaign_utilities\Controller;
 
+use DOMDocument;
+use DOMXPath;
+use Drupal\block_content\Entity\BlockContent;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\layout_builder\Section;
+use Drupal\node\Entity\Node;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -26,6 +32,7 @@ class NidirectCampaignCreatorController extends ControllerBase {
    */
   public function __construct(EntityTypeManagerInterface $entity_type_manager) {
     $this->entityTypeManager = $entity_type_manager;
+
   }
 
   /**
@@ -42,12 +49,87 @@ class NidirectCampaignCreatorController extends ControllerBase {
    */
   public function build($nid) {
 
+    // Load existing D7 content
+    $conn_drupal7 = Database::getConnection('default', 'migrate');
+
+
+    // Retrieve all landing pages from D7.
+    $query = $conn_drupal7->query(
+      "select body_value from {field_data_body} where entity_id = " . $nid);
+    $d7_landing_pages = $query->fetchCol(0);
+
+    // Create new landing page
+    $node = Node::create([
+      'type' => 'landing_page',
+      'title' => 'Careers',
+    ]);
+
+    // Parse the body content
+    $dom = new DOMDocument();
+    $dom->strictErrorChecking = FALSE;
+    $dom->preserveWhiteSpace = FALSE;
+    $dom->loadHTML($d7_landing_pages[0]);
+    $xpath = new DOMXPath($dom);
+
+    // Iterate each section and create a layout builder section
+    foreach ($xpath->query('/html/body/div') as $domnode) {
+
+      if ($domnode->hasAttribute('class')) {
+        $section_class = $domnode->getAttribute('class');
+
+        switch ($section_class) {
+          case 'three-cols';
+            $section = new Section('teasers_x3');
+            foreach ($xpath->query('div[contains(@class,\'col\')]/div[contains(@class,\'col-content\')]', $domnode) as $child) {
+              $block_content['title'] = $xpath->query('h2', $child)->item(0)->nodeValue;
+              $block_content['link'] = $xpath->query('h2/a', $child)->item(0)->getAttribute('href');
+              $block_content['body'] = $xpath->query('h2/following-sibling::p', $child)->item(0)->nodeValue;
+              $block = $this->createBlock('card_standard', $block_content);
+            }
+            break;
+          case 'two-cols';
+            $section = new Section('teasers_x2');
+            foreach ($xpath->query('div[contains(@class,\'col\')]/div[contains(@class,\'col-content\')]', $domnode) as $child) {
+            $block_content['title'] = $xpath->query('h2', $child)->item(0)->nodeValue;
+            $block_content['link'] = $xpath->query('h2/a', $child)->getAttribute('href');
+            $block_content['body'] = $xpath->query('h2/following-sibling::p', $child)->item(0)->nodeValue;
+            $block = $this->createBlock('card_standard', $block_content);
+          }
+            break;
+          default;
+            break;
+        }
+      }
+    }
+
+    // Iterate each column and create a new block
+
+    // save the landing page.
+
+    // Redirect to new page
+
     $build['content'] = [
       '#type' => 'item',
       '#markup' => $this->t('NID ') . $nid,
     ];
 
     return $build;
+  }
+
+  protected function createBlock($type, $content) {
+
+    $block = BlockContent::create([
+      'info' => $content['title'],
+      'type' => $type,
+      'langcode' => 'en',
+      'field_body' => $content['body'],
+      'field_teaser' => $content['teaser'],
+      'field_link' => $content['link'],
+      'title' => $content['title'],
+    ]);
+    $block->save();
+
+    return $block;
   }
 
 }

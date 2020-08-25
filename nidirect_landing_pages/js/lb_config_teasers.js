70d588aee7c043ea20d07fa39f6aabe1cc0ce8ad
DuttonMa <martin@theduttons.org>
2020-08-25 14:13:09 +0100

    JQuery working to control manual checkbox

--- nidirect_landing_pages/js/lb_config_teasers.js
+++ nidirect_landing_pages/js/lb_config_teasers.js
@@ -8,8 +8,13 @@
     attach: (context) => {
       // Check when the user clicks on the 'manual control' checkbox.
       $(context).find('[data-drupal-selector="edit-settings-block-form-field-manually-control-listing-value"]').on("change", function(){
-        alert('here we go');
-        console.log('fired');
+        if ($(context).find('[data-drupal-selector="edit-settings-block-form-field-manually-control-listing-value"]').prop("checked") == true) {
+          // Show teasers so that they can be manually controlled.
+          $(context).find('[data-drupal-selector="edit-settings-block-form-field-article-teasers-wrapper"]').show();
+        } else {
+          // Hide teasers as auto populated.
+          $(context).find('[data-drupal-selector="edit-settings-block-form-field-article-teasers-wrapper"]').hide();
+        }
       });
     }
   };

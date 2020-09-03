faeefc28f9bea0844ae72a8edcbd92e4020e5d36
Johan Gant <johan.gant@gmail.com>
2020-09-03 13:39:50 +0100

    Ditch redundant title check (for now)

--- nidirect_common/tests/src/Nightwatch/Tests/regressions/taxonomyAccess.js
+++ nidirect_common/tests/src/Nightwatch/Tests/regressions/taxonomyAccess.js
@@ -9,13 +9,12 @@ module.exports = {
 
   '[Regression] D8NID-847: Ensure that anonymous users can view taxonomy canonical page content': browser => {
 
-    // View the 'help' term page. It should show both a menu link item and not show 'Page not found'.
+    // View the 'help' term page. It should show a menu link item for this.
     browser.drupalRelativeURL('/taxonomy/term/402');
 
     const termName = 'Help';
 
     browser.expect.element('nav ul.nav-menu > li:nth-child(4)').text.to.equal(termName);
-    browser.expect.element('#main-content h1').text.to.not.equal('Page not found');
 
   }
 

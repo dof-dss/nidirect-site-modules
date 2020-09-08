1d86798335d3323bb16d8ccda83d5ea3f5a73077
omahm <hello@onemanandhismouse.com>
2020-09-08 16:40:17 +0100

    D8NID-866 ~ Extend original sugesstion class

--- nidirect_common/src/LinkitSuggestionManager.php
+++ nidirect_common/src/LinkitSuggestionManager.php
@@ -4,31 +4,22 @@ namespace Drupal\nidirect_common;
 
 /**
  * @file
- * Contains \Drupal\linkit\ResultManager.
+ * Contains \Drupal\nidirect_common\LinkitSuggestionManager.
  */
 
-use Drupal\Component\Utility\Html;
 use Drupal\Core\Url;
 use Drupal\linkit\ProfileInterface;
-use Drupal\linkit\Suggestion\DescriptionSuggestion;
 use Drupal\linkit\Suggestion\SimpleSuggestion;
 use Drupal\linkit\Suggestion\SuggestionCollection;
+use Drupal\linkit\SuggestionManager;
 
 /**
- * Result service to handle autocomplete matcher results.
+ * Replacement suggestion service to handle Linkit autocomplete results.
  */
-class LinkitSuggestionManager {
+class LinkitSuggestionManager extends SuggestionManager {
 
   /**
-   * Gets the results.
-   *
-   * @param \Drupal\linkit\ProfileInterface $linkitProfile
-   *   The linkit profile.
-   * @param string $search_string
-   *   The string ro use in the matchers.
-   *
-   * @return \Drupal\linkit\Suggestion\SuggestionCollection
-   *   A suggestion collection.
+   * {@inheritdoc}
    */
   public function getSuggestions(ProfileInterface $linkitProfile, string $search_string) {
 
@@ -43,17 +34,17 @@ class LinkitSuggestionManager {
       $suggestions->addSuggestion($suggestion);
     }
 
-    return $suggestions;
-  }
+    // Special for link to front page.
+    if (strpos($search_string, 'front') !== FALSE) {
+      $suggestion = new SimpleSuggestion();
+      $suggestion->setGroup('System');
+      $suggestion->setLabel('Front page: The front page for this site.');
+      $suggestion->setPath(Url::fromRoute('<front>')->toString());
 
-  public function addUnscathedSuggestion(SuggestionCollection $suggestionCollection, $search_string) {
-    $suggestion = new DescriptionSuggestion();
-    $suggestion->setLabel(Html::escape($search_string))
-      ->setGroup($this->t('No results'))
-      ->setDescription($this->t('Linkit could not find any suggestions. This URL will be used as is.'))
-      ->setPath($search_string);
-    $suggestionCollection->addSuggestion($suggestion);
-    return $suggestionCollection;
+      $suggestions->addSuggestion($suggestion);
+    }
+
+    return $suggestions;
   }
 
 }

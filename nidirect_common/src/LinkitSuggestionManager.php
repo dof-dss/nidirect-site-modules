fd12852dad4ec1cb430d5ef9e96c75242b94fb68
omahm <hello@onemanandhismouse.com>
2020-09-08 16:37:07 +0100

    D8NID-866 ~ Provide suggestions for empty searches

--- nidirect_common/src/LinkitSuggestionManager.php
+++ nidirect_common/src/LinkitSuggestionManager.php
@@ -27,55 +27,24 @@ class LinkitSuggestionManager {
    * @param string $search_string
    *   The string ro use in the matchers.
    *
-   * @return array
-   *   An array of matches.
+   * @return \Drupal\linkit\Suggestion\SuggestionCollection
+   *   A suggestion collection.
    */
   public function getSuggestions(ProfileInterface $linkitProfile, string $search_string) {
-    $matches = [];
 
+    $suggestions = new SuggestionCollection();
+
+    // Display for empty searches.
     if (empty(trim($search_string))) {
-      return [
-        [
-          'title' => t('No content results'),
-          'description' => t('Please enter search terms')
-        ]
-      ];
+      $suggestion = new SimpleSuggestion();
+      $suggestion->setGroup('No content results');
+      $suggestion->setLabel('Please enter search terms');
+
+      $suggestions->addSuggestion($suggestion);
     }
 
-    // Special for link to front page.
-    if (strpos($search_string, 'front') !== FALSE) {
-      $matches[] = [
-        'title' => t('Front page'),
-        'description' => 'The front page for this site.',
-        'path' => Url::fromRoute('<front>')->toString(),
-        'group' => t('System'),
-      ];
-    }
-
-    foreach ($linkitProfile->getMatchers() as $plugin) {
-      $matches = array_merge($matches, $plugin->getMatches($search_string));
-    }
-
-    // Check for an e-mail address then return an e-mail match and create a
-    // mail-to link if appropriate.
-    if (filter_var($search_string, FILTER_VALIDATE_EMAIL)) {
-      $matches[] = [
-        'title' => t('E-mail @email', ['@email' => $search_string]),
-        'description' => t('Creates a mailto link for e-mail @email', ['@email' => $search_string]),
-        'path' => 'mailto:' . Html::escape($search_string),
-        'group' => t('E-mail'),
-      ];
-    }
-
-    // If there is still no matches, return a "no results" array.
-    if (empty($matches)) {
-      return [
-        [
-          'title' => t('No content results'),
-          'description' => t('There were no content results. Using the link provided as-is.'),
-        ]
-      ];
-    }
+    return $suggestions;
+  }
 
   public function addUnscathedSuggestion(SuggestionCollection $suggestionCollection, $search_string) {
     $suggestion = new DescriptionSuggestion();

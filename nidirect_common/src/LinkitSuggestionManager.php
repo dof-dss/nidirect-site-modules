0881b9bcf8cf90ce813e4fbf92cc8af5fefca179
omahm <hello@onemanandhismouse.com>
2020-09-08 16:36:00 +0100

    D8NID-866 ~ Provide UnscathedSuggestion implementation

--- nidirect_common/src/LinkitSuggestionManager.php
+++ nidirect_common/src/LinkitSuggestionManager.php
@@ -10,6 +10,8 @@ namespace Drupal\nidirect_common;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Url;
 use Drupal\linkit\ProfileInterface;
+use Drupal\linkit\Suggestion\DescriptionSuggestion;
+use Drupal\linkit\Suggestion\SimpleSuggestion;
 use Drupal\linkit\Suggestion\SuggestionCollection;
 
 /**
@@ -75,7 +77,14 @@ class LinkitSuggestionManager {
       ];
     }
 
-    return $matches;
+  public function addUnscathedSuggestion(SuggestionCollection $suggestionCollection, $search_string) {
+    $suggestion = new DescriptionSuggestion();
+    $suggestion->setLabel(Html::escape($search_string))
+      ->setGroup($this->t('No results'))
+      ->setDescription($this->t('Linkit could not find any suggestions. This URL will be used as is.'))
+      ->setPath($search_string);
+    $suggestionCollection->addSuggestion($suggestion);
+    return $suggestionCollection;
   }
 
 }

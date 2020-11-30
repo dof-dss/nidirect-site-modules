86822178b982d0dc85f9d738bf26993cc05423cc
Johan Gant <johan.gant@gmail.com>
2020-11-30 12:04:35 +0000

    Inject news controller via service container

--- nidirect_news/src/Plugin/Block/FeaturedNewsBlock.php
+++ nidirect_news/src/Plugin/Block/FeaturedNewsBlock.php
@@ -3,7 +3,9 @@
 namespace Drupal\nidirect_news\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\node\NodeInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\nidirect_news\Controller\NewsListingController;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Block for presenting featured news; shared with news embed display
@@ -14,13 +16,49 @@ use Drupal\node\NodeInterface;
  *  admin_label = @Translation("Featured news"),
  * )
  */
-class FeaturedNewsBlock extends BlockBase {
+class FeaturedNewsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The news controller service.
+   *
+   * @var Drupal\nidirect_news\Controller\NewsListingController
+   */
+  protected $newsService;
+
+  /**
+   * ContactAzBlock constructor.
+   *
+   * @param array $configuration
+   *   Site configuration.
+   * @param string $plugin_id
+   *   The plugin id.
+   * @param mixed $plugin_definition
+   *   Plugin definition.
+   * @param \Drupal\nidirect_news\Controller\NewsListingController $news_service
+   *   News controller service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, NewsListingController $news_service) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->newsService = $news_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('nidirect_news.news')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $view = \Drupal::service('nidirect_news.news')->getLatestNewsView();
+    $view = $this->newsService->getLatestNewsView();
     $content['featured_news'] = $view->buildRenderable('latest_news_block');
 
     return $content;

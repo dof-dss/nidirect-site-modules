b1636f36c2e537c50744a60f5636f05d63cf53b3
omahm <hello@onemanandhismouse.com>
2020-12-03 13:59:19 +0000

    D8NID-995 ~ Load services and weather station entities

--- nidirect_cold_weather_payments/tests/src/Kernel/ColdWeatherPaymentsTest.php
+++ nidirect_cold_weather_payments/tests/src/Kernel/ColdWeatherPaymentsTest.php
@@ -5,12 +5,27 @@ namespace Drupal\Tests\nidirect_cold_weather_payments\Kernel;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
- * Cold Weather Payments tests
+ * Cold Weather Payments tests.
  *
  * @group nidirect_cold_weather_payments
  * @group nidirect
  */
 class ColdWeatherPaymentsTest extends KernelTestBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+
+  /**
+   * List of Weather station entities.
+   *
+   * @var array
+   */
+  protected $stations;
 
   /**
    * {@inheritdoc}
@@ -31,8 +46,12 @@ class ColdWeatherPaymentsTest extends KernelTestBase {
   protected function setUp() {
     parent::setUp();
 
+    $this->installEntitySchema('weather_station');
     $this->installConfig(['nidirect_cold_weather_payments']);
-    $this->installEntitySchema('cold_weather_payment');
+
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+
+    $this->stations = $this->entityTypeManager->getStorage('weather_station')->loadMultiple();
   }
 
 }

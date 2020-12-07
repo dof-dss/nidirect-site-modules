ba79eb66723bb7de1ff860e1e80165fd40d14cf7
omahm <hello@onemanandhismouse.com>
2020-12-07 14:48:41 +0000

    D8NID-227 ~ Test for postcode without payment

--- nidirect_cold_weather_payments/tests/src/Kernel/ColdWeatherPaymentsTest.php
+++ nidirect_cold_weather_payments/tests/src/Kernel/ColdWeatherPaymentsTest.php
@@ -155,4 +155,49 @@ class ColdWeatherPaymentsTest extends EntityKernelTestBase {
     ], $result['payments_triggered'][2]);
   }
 
+  /**
+   * Tests that payment details are not returned for a postcode.
+   */
+  public function testPostcodeHasNoPayment() {
+
+    // Postcode that matches the 'Stormont' station.
+    $postcode = 'BT15 1AB';
+
+    $result = $this->paymentsService->forPostcode($postcode);
+
+    self::assertEquals([
+      'date_start' => '2020-12-03',
+      'date_end' => '2020-12-17',
+      'stations' => 'aldergrove,glenanne,magilligan',
+      'postcodes' => [
+        '27','28','29','39','40','41','42','43','44',
+        '45','46','80','35','60','61','62','63','64',
+        '65','66','67','68','69','70','71','47','48',
+        '49','51','52','53','54','55','56','57'],
+      'payment_granted' => false,
+
+    ], $result['payments_triggered'][0]);
+
+    self::assertEquals([
+      'date_start' => '2020-12-10',
+      'date_end' => '2020-12-22',
+      'stations' => 'katesbridge',
+      'postcodes' => [
+        '24','25','26','30','31','32','33','34'],
+      'payment_granted' => false,
+
+    ], $result['payments_triggered'][1]);
+
+    self::assertEquals([
+      'date_start' => '2020-12-22',
+      'date_end' => '2020-12-28',
+      'stations' => 'aldergrove',
+      'postcodes' => [
+        '27','28','29','39','40','41','42','43','44',
+        '45','46','80'],
+      'payment_granted' => false,
+
+    ], $result['payments_triggered'][2]);
+  }
+
 }

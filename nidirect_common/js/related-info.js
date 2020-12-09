40ad65ebaef1a24a065a34d735b8136f8994c352
omahm <hello@onemanandhismouse.com>
2020-12-09 12:40:15 +0000

    D8NID-931 ~ Extract and process title from URL field text before copying to title field

--- nidirect_common/js/related-info.js
+++ nidirect_common/js/related-info.js
@@ -1,1 +1,27 @@
-console.log('Related info');
+/**
+ * @file
+ * Defines Javascript behaviors for Related Info / More Useful links fields.
+ */
+(function ($) {
+
+  "use strict";
+
+  /**
+   * Extracts and copies the title of internal nodes from the URL input to the Link text input.
+   */
+  Drupal.behaviors.relatedInfo = {
+    attach: function (context, settings) {
+
+      // Match nid which is the last occurrence of open bracket, number, close bracket.
+      const regex = /\(\d+\)$/gm;
+
+      $('.field--name-field-related-info input.form-autocomplete').autocomplete({
+        close: function( event, ui ) {
+          let title = this.value.replace(regex, '');
+          $(this).parent().next().children('input').val(title);
+        }
+      });
+    }
+  };
+
+}(jQuery, Drupal));

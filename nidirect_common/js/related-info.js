76f0646b65b28ba5790ac6a794d354537715b792
omahm <hello@onemanandhismouse.com>
2020-12-09 13:40:01 +0000

    D8NID-931 ~ Only process internal URLs

--- nidirect_common/js/related-info.js
+++ nidirect_common/js/related-info.js
@@ -11,14 +11,16 @@
    */
   Drupal.behaviors.relatedInfo = {
     attach: function (context, settings) {
-
       // Match nid which is the last occurrence of open bracket, number, close bracket.
       const regex = /\(\d+\)$/gm;
 
       $('.field--name-field-related-info input.form-autocomplete').autocomplete({
-        close: function( event, ui ) {
-          let title = this.value.replace(regex, '');
-          $(this).parent().next().children('input').val(title);
+        close: function(event, ui) {
+          // Only process the title if we are dealing with internal urls.
+          if (this.value.startsWith('http://') === false || this.value.startsWith('https://') === false) {
+            let title = this.value.replace(regex, '');
+            $(this).parent().next().children('input').val(title);
+          }
         }
       });
     }

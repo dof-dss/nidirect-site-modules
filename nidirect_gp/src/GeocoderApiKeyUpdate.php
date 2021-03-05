f02bdc26a100b722ea4a7779e373dd83ab5f3d4d
DuttonMa <martin@theduttons.org>
2021-03-05 11:46:39 +0000

    code improvements

--- nidirect_gp/src/GeocoderApiKeyUpdate.php
+++ nidirect_gp/src/GeocoderApiKeyUpdate.php
@@ -8,47 +8,40 @@ use Drupal\Core\Config\ConfigImporterEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * A subscriber invalidating cache tags when color config objects are saved.
+ * A subscriber that overwrites Google Maps api keys on config import.
  */
 class GeocoderApiKeyUpdate implements EventSubscriberInterface {
 
   /**
-   * Invalidate cache tags when a color theme config object changes.
+   * Overwrite Google Maps API key when it changes.
    *
    * @param \Drupal\Core\Config\ConfigImporterEvent $event
    *   The Event to process.
    */
-  public function onChange(ConfigImporterEvent $event) {
-    // Changing a theme's color settings causes the theme's asset library
-    // containing the color CSS file to be altered to use a different file.
-    \Drupal::logger('nidirect_gp')->notice(t("checking for geocoder"));
-    if (($event->getChangelist()['update'][0] == 'geocoder.geocoder_provider.googlemaps') ||
-      ($event->getChangelist()['create'][0] == 'geocoder.geocoder_provider.googlemaps')) {
-      $config_factory = \Drupal::configFactory();
+  public function onProviderChange(ConfigImporterEvent $event) {
+    // We need to set up a Geocoder provider in config but we can't store a Google Maps API key
+    // in config so we just put a placeholder in. When the geocoder config is imported or
+    // updated code has been added here to overwrite the API key with the one stored in
+    // the environment variable.
+    $change_list = $event->getChangelist();
+    if (!empty($change_list)) {
+      if ((isset($change_list['update']) && ($change_list['update'][0] == 'geocoder.geocoder_provider.googlemaps')) ||
+        (isset($change_list['create']) && ($change_list['create'][0] == 'geocoder.geocoder_provider.googlemaps'))) {
+        $config_factory = \Drupal::configFactory();
 
-      // Retrieve the geocoder provider settings.
-      $googlemap_config = $config_factory->getEditable('geocoder.geocoder_provider.googlemaps')->get('configuration');
+        // Retrieve the geocoder provider settings.
+        $googlemap_config = $config_factory->getEditable('geocoder.geocoder_provider.googlemaps')->get('configuration');
 
-      if (!empty($googlemap_config) && isset($googlemap_config['apiKey'])) {
-        // Overwrite the google map api key.
-        \Drupal::logger('nidirect_gp')->notice(t("xxSuccessfully overwrote Googlemap api key"));
-        $googlemap_config['apiKey'] = getenv('GOOGLE_MAP_API_SERVER_KEY');
-        $config_factory->getEditable('geocoder.geocoder_provider.googlemaps')->set('configuration', $googlemap_config)->save();
-      } else {
-        $message = "Googlemap Geocoder provider not found at /admin/config/system/geocoder/geocoder-provider";
-        \Drupal::logger('nidirect_gp')->error(t($message));
+        if (!empty($googlemap_config) && isset($googlemap_config['apiKey'])) {
+          // Overwrite the google map api key.
+          $googlemap_config['apiKey'] = getenv('GOOGLE_MAP_API_SERVER_KEY');
+          $config_factory->getEditable('geocoder.geocoder_provider.googlemaps')->set('configuration', $googlemap_config)->save();
+        } else {
+          $message = "Googlemap Geocoder provider not found at /admin/config/system/geocoder/geocoder-provider";
+          \Drupal::logger('nidirect_gp')->error(t($message));
+        }
       }
     }
-  }
-
-  /**
-   * Creates a config snapshot.
-   *
-   * @param \Drupal\Core\Config\ConfigImporterEvent $event
-   *   The Event to process.
-   */
-  public function onConfigImporterImport(ConfigImporterEvent $event) {
-    $this->configManager->createSnapshot($this->sourceStorage, $this->snapshotStorage);
   }
 
   /**
@@ -58,8 +51,7 @@ class GeocoderApiKeyUpdate implements EventSubscriberInterface {
    *   An array of event listener definitions.
    */
   public static function getSubscribedEvents() {
-    \Drupal::logger('nidirect_gp')->notice(t("getsub"));
-    $events[ConfigEvents::IMPORT][] = ['onChange', 40];
+    $events[ConfigEvents::IMPORT][] = ['onProviderChange', 40];
     return $events;
   }
 

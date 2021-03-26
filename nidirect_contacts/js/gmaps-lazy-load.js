689cf97ff848079f9123e82757b3b68537873806
omahm <hello@onemanandhismouse.com>
2021-03-26 10:56:03 +0000

    D8NID-1124 ~ Maps URL changed to API query to allow for marker display

--- nidirect_contacts/js/gmaps-lazy-load.js
+++ nidirect_contacts/js/gmaps-lazy-load.js
@@ -38,7 +38,7 @@
               // user has given consent to load Google maps, if they haven't
               // prevent lazy loading of the map and replace with a text link.
               if (Drupal.eu_cookie_compliance != undefined && Drupal.eu_cookie_compliance.hasAgreed() == false) {
-                  let url = 'https://www.google.com/maps/@' + mapLatLng.lat + ',' + mapLatLng.lng + ',' + mapSettings.zoom + 'z';
+                  let url = 'https://www.google.com/maps/search/?api=1&query=' + mapLatLng.lat + ',' + mapLatLng.lng;
                   let map = $('.' + entry.target.id);
                   // Replace map div contents with a text link to Google maps.
                   map.html('<a href="' + url + '" target="_blank" rel="noopener noreferrer">View this location on Google Maps' +
@@ -46,7 +46,7 @@
                     '<svg aria-hidden="true" class="ico ico-elink"><title>external link opens in a new window / tab</title>' +
                     '<use xlink:href="#elink"></use></svg></a>');
                   // Override the gmap class which sets the height of the map div to 400px.
-                  map.css('height', 'auto');q
+                  map.css('height', 'auto');
 
                   // Unsubscribe this target from the observer.
                   observer.unobserve(entry.target);

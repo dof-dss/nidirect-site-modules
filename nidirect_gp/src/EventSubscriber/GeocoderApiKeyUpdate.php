3218488d7018983c6550d0bb78fcef42864782a0
DuttonMa <martin@theduttons.org>
2021-05-20 15:27:00 +0100

    dependency injection

--- nidirect_gp/src/EventSubscriber/GeocoderApiKeyUpdate.php
+++ nidirect_gp/src/EventSubscriber/GeocoderApiKeyUpdate.php
@@ -7,12 +7,30 @@ use Drupal\Core\Config\ConfigCrudEvent;
 use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Config\ConfigImporterEvent;
 use Drupal\Core\Config\Importer\MissingContentEvent;
+use Drupal\nidirect_common\UpdateConfigFromEnvironment;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * A subscriber that overwrites Google Maps api keys on config import.
  */
 class GeocoderApiKeyUpdate implements EventSubscriberInterface {
+
+  /**
+   * The upadte config from env service.
+   *
+   * @var Drupal\nidirect_common\UpdateConfigFromEnvironment
+   */
+  protected $updateEnvService;
+
+  /**
+   * Constructs a new GeocoderApiKeyUpdate instance.
+   *
+   * @param Drupal\nidirect_common\UpdateConfigFromEnvironment $update_service
+   *   The entity type manager.
+   */
+  public function __construct(UpdateConfigFromEnvironment $update_service) {
+    $this->updateEnvService = $update_service;
+  }
 
   /**
    * Registers the methods in this class that should be listeners.
@@ -42,8 +60,7 @@ class GeocoderApiKeyUpdate implements EventSubscriberInterface {
     if (!empty($change_list)) {
       if ((isset($change_list['update']) && ($change_list['update'][0] == 'geocoder.geocoder_provider.googlemaps')) ||
         (isset($change_list['create']) && ($change_list['create'][0] == 'geocoder.geocoder_provider.googlemaps'))) {
-        $config_update_service = \Drupal::service('nidirect_common.update_config_from_environment');
-        $config_update_service->updateApiKey('geocoder.geocoder_provider.googlemaps', 'apiKey', 'GOOGLE_MAP_API_SERVER_KEY');
+        $this->updateEnvService->updateApiKey('geocoder.geocoder_provider.googlemaps', 'apiKey', 'GOOGLE_MAP_API_SERVER_KEY');
       }
     }
   }

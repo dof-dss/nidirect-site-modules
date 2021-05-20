fa1cb8121be6d618ce06f45ee3700baa58c87aa0
DuttonMa <martin@theduttons.org>
2021-05-20 09:00:19 +0100

    further attempts to trigger event on empty import

--- nidirect_gp/src/EventSubscriber/GeocoderApiKeyUpdate.php
+++ nidirect_gp/src/EventSubscriber/GeocoderApiKeyUpdate.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\nidirect_gp\EventSubscriber;
 
+use Drupal\Core\Config\ConfigCollectionInfo;
+use Drupal\Core\Config\ConfigCrudEvent;
 use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Config\ConfigImporterEvent;
+use Drupal\Core\Config\Importer\MissingContentEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -18,10 +21,76 @@ class GeocoderApiKeyUpdate implements EventSubscriberInterface {
    *   An array of event listener definitions.
    */
   public static function getSubscribedEvents() {
-    $events[ConfigEvents::IMPORT][] = ['onProviderChange', 40];
-    $events[ConfigEvents::IMPORT_VALIDATE][] = ['onEmptyImport', 40];
+    return array(
+      ConfigEvents::IMPORT => array('onProviderChange'),
+      ConfigEvents::IMPORT_VALIDATE => array('onEmptyImport'),
+      ConfigEvents::COLLECTION_INFO => array('addCollections'),
+      ConfigEvents::IMPORT_MISSING_CONTENT => array('onMissingContent')
+    );
+    //$events[ConfigEvents::SAVE][] = ['onConfigSave', 40];
+    //$events[ConfigEvents::IMPORT][] = ['onProviderChange', 40];
+    //$events[ConfigEvents::IMPORT_VALIDATE][] = ['onEmptyImport', 40];
     \Drupal::logger('nidirect_gp')->notice(t("registering for config import event"));
     return $events;
+  }
+
+  /**
+   * Handles the missing content event.
+   *
+   * @param \Drupal\Core\Config\Importer\MissingContentEvent $event
+   *   The missing content event.
+   */
+  public function onMissingContent(MissingContentEvent $event) {
+    \Drupal::logger('nidirect_gp')->notice(t("FIRING missing content event !"));
+    $config_update_service = \Drupal::service('nidirect_common.update_config_from_environment');
+    $config_update_service->updateApiKey('geocoder.geocoder_provider.googlemaps', 'apiKey', 'GOOGLE_MAP_API_SERVER_KEY');
+  }
+
+  /**
+   * Reacts to the ConfigEvents::COLLECTION_INFO event.
+   *
+   * @param \Drupal\Core\Config\ConfigCollectionInfo $collection_info
+   *   The configuration collection info event.
+   */
+  public function addCollections(ConfigCollectionInfo $collection_info) {
+    \Drupal::logger('nidirect_gp')->notice(t("FIRING add collection import event !"));
+    $config_update_service = \Drupal::service('nidirect_common.update_config_from_environment');
+    $config_update_service->updateApiKey('geocoder.geocoder_provider.googlemaps', 'apiKey', 'GOOGLE_MAP_API_SERVER_KEY');
+  }
+
+
+  /**
+   * Overwrite Google Maps API key before config import.
+   *
+   * @param \Drupal\Core\Config\ConfigImporterEvent $event
+   *   The Event to process.
+   */
+  public function onEmptyImport(ConfigImporterEvent $event) {
+    // We need to set up a Geocoder provider in config but we can't store a
+    // Google Maps API key in config so we just put a placeholder in.
+    // When the geocoder config is imported or updated code has been added
+    // here to overwrite the API key with the one stored in the environment
+    // variable.
+    \Drupal::logger('nidirect_gp')->notice(t("FIRING empty import event !"));
+    $config_update_service = \Drupal::service('nidirect_common.update_config_from_environment');
+    $config_update_service->updateApiKey('geocoder.geocoder_provider.googlemaps', 'apiKey', 'GOOGLE_MAP_API_SERVER_KEY');
+  }
+
+  /**
+   * Overwrite Google Maps API key before config import.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   *   The Event to process.
+   */
+  public function onSaveImport(ConfigCrudEvent $event) {
+    // We need to set up a Geocoder provider in config but we can't store a
+    // Google Maps API key in config so we just put a placeholder in.
+    // When the geocoder config is imported or updated code has been added
+    // here to overwrite the API key with the one stored in the environment
+    // variable.
+    \Drupal::logger('nidirect_gp')->notice(t("FIRING Save import event !"));
+    $config_update_service = \Drupal::service('nidirect_common.update_config_from_environment');
+    $config_update_service->updateApiKey('geocoder.geocoder_provider.googlemaps', 'apiKey', 'GOOGLE_MAP_API_SERVER_KEY');
   }
 
   /**

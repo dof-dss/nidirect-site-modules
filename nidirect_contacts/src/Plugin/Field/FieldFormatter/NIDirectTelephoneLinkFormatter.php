7389fca5987ea520ee638f6200e289406c8e2d73
omahm <hello@onemanandhismouse.com>
2021-06-08 21:45:03 +0100

    D8NID-1214 ~ Refactored to alter existing plugin output

--- nidirect_contacts/src/Plugin/Field/FieldFormatter/NIDirectTelephoneLinkFormatter.php
+++ nidirect_contacts/src/Plugin/Field/FieldFormatter/NIDirectTelephoneLinkFormatter.php
@@ -25,107 +25,19 @@ class NIDirectTelephoneLinkFormatter extends TelephonePlusLinkFormatter {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = [];
+    $elements = parent::viewElements($items, $langcode);
 
-    foreach ($items as $delta => $item) {
-      $title = NULL;
-      $supplementary = NULL;
-
-      $telephone = new TelephonePlusValidator($item->telephone_number, $item->telephone_extension, $item->country_code);
-
-      // If we don't have a valid number, set variables to allow fallback to
-      // plain text.
-      if (!$telephone->isValid()) {
-        $telephone_text = $item->telephone_number;
-        $telephone_link = '';
-      }
-      else {
-        $telephone = new TelephonePlusFormatter($item->telephone_number, $item->telephone_extension, $item->country_code);
-        // TelephonePlus link text.
-        $telephone_link = $telephone->url();
-        // TelephonePlus display text.
-        $telephone_text = $telephone->text($item->display_international_number);
-      }
-
-      // Add extension to TelephonePlus display text if there is one.
-      if ($item->telephone_extension) {
-        $telephone_text .= ' ' . t('ext. :extension', [':extension' => $item->telephone_extension]);
-      }
-
-      // Use the raw textfield input for textphone formatting.
-      if (substr($telephone_text, 0, 5 ) === "18001") {
-        $telephone_text = $item->getValue('telephone_number')['telephone_number'];
-      }
+    foreach ($items as $item) {
+      $matches = [];
 
       // Check for international freephone numbers and reformat the output
-      if (substr($item->getValue('telephone_number')['telephone_number'], 0, 6 ) === "00 800") {
-        $telephone_text = $item->getValue('telephone_number')['telephone_number'];
-      }
-
-      if (!empty($item->telephone_title)) {
-        $title = [
-          '#type' => 'html_tag',
-          '#tag' => 'span',
-          '#attributes' => ['class' => ['title']],
-          '#value' => $item->telephone_title,
-        ];
-
-        // Add vCard title if enabled.
-        if ($this->getSetting('vcard')) {
-          $title['#attributes']['class'][] = 'type';
+      if (preg_match('/^00\s?800\s?(.+)/m', $item->getValue('telephone_number')['telephone_number'], $matches)) {
+        foreach ($elements as &$element) {
+          if (strpos($element['number']['#value'], $matches[1]) !== FALSE) {
+            $element['number']['#value'] = $item->getValue('telephone_number')['telephone_number'];
+          }
         }
       }
-
-      if (!empty($item->telephone_supplementary)) {
-        $supplementary = [
-          '#type' => 'html_tag',
-          '#tag' => 'span',
-          '#attributes' => ['class' => ['supplementary']],
-          '#value' => $item->telephone_supplementary,
-        ];
-      }
-
-      if (!empty($telephone_link)) {
-        // Url::fromUri() doesn't place nice with the generated tel: URI
-        // so reverting to using an html_tag instead of a link element.
-        $phone = [
-          '#type' => 'html_tag',
-          '#tag' => 'a',
-          '#attributes' => ['href' => $telephone_link],
-          '#value' => $telephone_text,
-        ];
-      }
-      else {
-        $phone = [
-          '#type' => 'html_tag',
-          '#tag' => 'span',
-          '#value' => $telephone_text,
-        ];
-      }
-
-      if ($item->telephone_extension) {
-        $phone['#suffix'] = ' ' . t('extension  :extension', [':extension' => $item->telephone_extension]);
-      }
-
-      // If vCard option is enabled and add required classes and
-      // enclose in div element with 'tel' class.
-      if ($this->getSetting('vcard')) {
-        $phone['#attributes']['class'][] = 'value';
-        if (!isset($item->_attributes)) {
-          $item->_attributes = [];
-        }
-        $item->_attributes += ['class' => ['tel']];
-      }
-
-      if ($title) {
-        $elements[$delta]['title'] = $title;
-      }
-
-      if ($supplementary) {
-        $elements[$delta]['supplementary'] = $supplementary;
-      }
-
-      $elements[$delta]['number'] = $phone;
     }
 
     return $elements;

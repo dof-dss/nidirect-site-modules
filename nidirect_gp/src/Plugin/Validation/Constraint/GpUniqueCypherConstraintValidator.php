b0002064404d369bad75b95c2695fa432e0431af
omahm <hello@onemanandhismouse.com>
2021-06-29 16:21:55 +0100

    D8NID-1309 ~ Constraint validator for unique GP cypher

---
+++ nidirect_gp/src/Plugin/Validation/Constraint/GpUniqueCypherConstraintValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\nidirect_gp\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates that a GP has a unique cypher.
+ */
+class GpUniqueCypherConstraintValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($entity, Constraint $constraint) {
+    if (!isset($entity) ||  !$cypher = $entity->getCypher()) {
+      return;
+    }
+
+    $is_unique = \Drupal::service('nidirect_gp.unique_cypher')->isCypherUnique($cypher, [$entity->id()]);
+
+    if ($is_unique === FALSE) {
+      $this->context->addViolation($constraint->message);
+    }
+  }
+
+}

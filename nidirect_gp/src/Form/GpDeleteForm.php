b6c3b46878dcc22fdbe6d1b3511ff5ae6b686fbc
OMAHM <hello@onemanandhismouse.com>
2022-06-28 16:48:48 +0100

    D8NID-1532 ~ Remove deleted GP from lead gp in practice

--- nidirect_gp/src/Form/GpDeleteForm.php
+++ nidirect_gp/src/Form/GpDeleteForm.php
@@ -34,6 +34,7 @@ class GpDeleteForm extends ContentEntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $gpid =$this->getEntity()->id();
 
     // Fetch all GP Practices so we can check for references to this GP.
     $gp_practices = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(
@@ -41,14 +42,17 @@ class GpDeleteForm extends ContentEntityConfirmFormBase {
       ]);
 
     foreach ($gp_practices as $gp_practice) {
-
-      // TODO: Deal with lead GP reference (waiting on ticket clarification)
+      // Remove if lead GP for a practice.
+      if ($gp_practice->get('field_gp_practice_lead')->getString() == $gpid) {
+        $gp_practice->get('field_gp_practice_lead')->removeItem(0);
+        $gp_practice->save();
+      }
 
       // Remove GP from practice members list.
       $members = $gp_practice->get('field_gp_practice_member')->referencedEntities();
 
       foreach ($members as $index => $member) {
-        if ($member->id() == $this->getEntity()->id()) {
+        if ($member->id() == $gpid) {
           $gp_practice->get('field_gp_practice_member')->removeItem($index);
           $gp_practice->save();
         }
